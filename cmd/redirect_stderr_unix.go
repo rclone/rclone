a00b5066ab86a1fb3fb1607dd2fc11a673c46c78
Fredrik Fornwall <fredrik@fornwall.net>
2016-09-07 22:21:58 +0200

    Use Dup2 library function instead of raw syscall
    The Dup2 syscall does not exist on 64-bit arm Linux while its
    replacement Dup3 does not exist on non-Linux systems.
    
    Using the unix.Dup2 library function instead of raw syscalls
    improves the portability across more platforms.

--- cmd/redirect_stderr_unix.go
+++ cmd/redirect_stderr_unix.go
@@ -7,12 +7,13 @@ package cmd
 import (
 	"log"
 	"os"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // redirectStderr to the file passed in
 func redirectStderr(f *os.File) {
-	err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+	err := unix.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
 	if err != nil {
 		log.Fatalf("Failed to redirect stderr to file: %v", err)
 	}
