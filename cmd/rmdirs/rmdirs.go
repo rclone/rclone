8aae166a5b1bf926e8f2b3082e810181391d857a
Nick Craig-Wood <nick@craig-wood.com>
2016-11-27 18:36:13 +0000

    Add missing rmdirs command

---
+++ cmd/rmdirs/rmdirs.go
@@ -0,0 +1,31 @@
+package rmdir
+
+import (
+	"github.com/ncw/rclone/cmd"
+	"github.com/ncw/rclone/fs"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cmd.Root.AddCommand(rmdirsCmd)
+}
+
+var rmdirsCmd = &cobra.Command{
+	Use:   "rmdirs remote:path",
+	Short: `Remove any empty directoryies under the path.`,
+	Long: `This removes any empty directories (or directories that only contain
+empty directories) under the path that it finds, including the path if
+it has nothing in.
+
+This is useful for tidying up remotes that rclone has left a lot of
+empty directories in.
+
+`,
+	Run: func(command *cobra.Command, args []string) {
+		cmd.CheckArgs(1, 1, command, args)
+		fdst := cmd.NewFsDst(args)
+		cmd.Run(true, command, func() error {
+			return fs.Rmdirs(fdst)
+		})
+	},
+}
