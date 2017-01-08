45a71595fb319efd31c0c975019947544d8fbf1f
T.C. Ferguson <tcf909@gmail.com>
2017-01-08 10:24:03 -0600

    Update amazonclouddrive.md
    Added details about Amazon Drive's latest trash retention policy.
    See:
    https://www.reddit.com/r/DataHoarder/comments/5dh96j/files_in_amazon_cloud_drive_trash_now_deleted/
    https://www.amazon.com/gp/help/customer/display.html?nodeId=201376760

--- cmd/all/all.go
+++ cmd/all/all.go
@@ -25,6 +25,7 @@ import (
 	_ "github.com/ncw/rclone/cmd/mount"
 	_ "github.com/ncw/rclone/cmd/move"
 	_ "github.com/ncw/rclone/cmd/moveto"
+	_ "github.com/ncw/rclone/cmd/obscure"
 	_ "github.com/ncw/rclone/cmd/purge"
 	_ "github.com/ncw/rclone/cmd/rmdir"
 	_ "github.com/ncw/rclone/cmd/rmdirs"
