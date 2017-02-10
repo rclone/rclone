13d5e1927e6bec06aeef6e9d2b1e9b91b5900aab
Dedsec1 <Dedsec1@users.noreply.github.com>
2017-02-09 23:24:19 -0500

    Create rclone.sh

---
+++ rclone.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# [Quick Box :: Install rclone]
+#
+# GITHUB REPOS
+# GitHub _ packages  :   https://github.com/QuickBox/quickbox_packages
+# LOCAL REPOS
+# Local _ packages   :   ~/QuickBox/packages
+# Author             :   DedSec
+# URL                :   https://plaza.quickbox.io
+#
+# QuickBox Copyright (C) 2016 QuickBox.io
+# Licensed under GNU General Public License v3.0 GPL-3 (in short)
+#
+#   You may copy, distribute and modify the software as long as you track
+#   changes/dates in source files. Any modifications to our software
+#   including (via compiler) GPL-licensed code must also be made available
+#   under the GPL along with build & install instructions.
+
+go get -u -v github.com/ncw/rclone/...
+go get -u -v github.com/ncw/rclone
+
