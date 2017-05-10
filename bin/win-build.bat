79aa060e2112a19e36968bdb39104e76ad7465b7
Nick Craig-Wood <nick@craig-wood.com>
2017-05-10 11:20:38 +0100

    win-build.bat example bat file for building with WinFsp under Windows

---
+++ bin/win-build.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting environment variables for mingw+WinFsp compile
+set GOPATH=X:\go
+set PATH=C:\Program Files\mingw-w64\i686-7.1.0-win32-dwarf-rt_v5-rev0\mingw32\bin;%PATH%
+set CPATH=C:\Program Files\WinFsp\inc\fuse
