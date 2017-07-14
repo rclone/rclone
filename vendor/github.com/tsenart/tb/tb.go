ec6c3f2686a8b053e18d8531b726465e762b6e35
Nick Craig-Wood <nick@craig-wood.com>
2017-07-14 05:34:18 +0100

    vendor: remove github.com/tsenart/tb

--- vendor/github.com/tsenart/tb/tb.go
+++
@@ -1,4 +0,0 @@
-// Package tb provides a generic lock-free implementation of the
-// Token Bucket algorithm where non-conformity is handled by the user.
-// http://en.wikipedia.org/wiki/Token_bucket
-package tb
