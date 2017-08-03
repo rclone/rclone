0971adfa615be0713f28dd8f99e7b4cfb88ac0b0
Oliver Heyme <olihey@googlemail.com>
2017-08-03 20:08:31 +0200

    oauthutil: Added AuthOptions and shuts down the web server properly
    1. This makes AuthOptions a parameter for doConfig, Config and ConfigOffline to enable a Fs to add additional options (required for OneDrive for Business)
    2. Fix to properly shutdown the webserver recieving the auth information (go1.8)

---
+++ oauthutil/oauthutil_old.go
@@ -0,0 +1,16 @@
+// oauthutil parts pre go1.8+
+
+//+build !go1.8
+
+package oauthutil
+
+import "github.com/ncw/rclone/fs"
+
+func (s *authServer) Stop() {
+	fs.Debugf(nil, "Closing auth server")
+	if s.code != nil {
+		close(s.code)
+		s.code = nil
+	}
+	_ = s.listener.Close()
+}
