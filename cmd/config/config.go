0575623dffd60a064be60227ea27851b3cd3113d
thierry <thierry@substantiel.fr>
2017-09-21 23:27:39 +0200

    Add config listproviders, listoptions, jsonconfig for automated config
    Addition of a method listing the providers, a method listing the
    options of a provider and method of manual configuration.

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -26,23 +26,40 @@ Additional functions:
   * ` + "`rclone config edit`" + ` – same as above
   * ` + "`rclone config file`" + ` – show path of configuration file in use
   * ` + "`rclone config show`" + ` – print (decrypted) config file
+  * ` + "`rclone config listproviders`" + ` – List, in json format, the protocols supported by sync
+  * ` + "`rclone config listoptions type`" + ` – Lists all the options needed to connect to a protocol
+  * ` + "`rclone config jsonconfig`name type jsonoptions" + ` – Created a new remote type X with parameters Y
 `,
 	Run: func(command *cobra.Command, args []string) {
-		cmd.CheckArgs(0, 1, command, args)
+		cmd.CheckArgs(0, 4, command, args)
 		if len(args) == 0 {
 			fs.EditConfig()
-			return
+		} else if (len(args) == 1) {
+			switch args[0] {
+			case "edit":
+				fs.EditConfig()
+			case "show":
+				fs.ShowConfig()
+			case "file":
+				fs.ShowConfigLocation()
+			case "listproviders":
+				fs.ListProviders()
+			default:
+				fmt.Fprintf(os.Stderr, "Unknown subcommand %q, %s only supports edit, show and file.\n", args[0], command.Name())
+			}
+		} else if (len(args) == 2) {
+			if ((args[0] == "listoptions") && (args[1] != "")) {
+				fs.ListOptions(args[1])
+			} else {
+				fmt.Fprintf(os.Stderr, "Unknown subcommand %q %q, %s only supports optionsprovider <type>.\n", args[0], args[1], command.Name())
+			}
+		} else if (len(args) == 4) {
+			if ((args[0] == "jsonconfig") && (args[1] != "") && (args[2] != "") && (args[3]!= "")) {
+				fs.JsonConfig(args[1], args[2], args[3])
+			} else {
+				fmt.Fprintf(os.Stderr, "Unknown subcommand %q %q %q %q, %s only supports jsonconfig <name> <type> <json:options>.\n", args[0], args[1], args[2], args[3], command.Name())
+		    }
 		}
-
-		switch args[0] {
-		case "edit":
-			fs.EditConfig()
-		case "show":
-			fs.ShowConfig()
-		case "file":
-			fs.ShowConfigLocation()
-		default:
-			fmt.Fprintf(os.Stderr, "Unknown subcommand %q, %s only supports edit, show and file.\n", args[0], command.Name())
-		}
+		return
 	},
 }
