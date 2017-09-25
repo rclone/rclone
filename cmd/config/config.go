ec0918547bdb0ff29e85dded0c6ee5e5b2e8ded0
Substantiel <thierry@substantiel.fr>
2017-09-25 20:38:17 +0200

    Update config.go
    Long description correction

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -28,7 +28,7 @@ Additional functions:
   * ` + "`rclone config show`" + ` – print (decrypted) config file
   * ` + "`rclone config listproviders`" + ` – List, in json format, the protocols supported by sync
   * ` + "`rclone config optionsprovider type`" + ` – Lists all the options needed to connect to a protocol
-  * ` + "`rclone config jsonconfig`" + ` – print (decrypted) config file
+  * ` + "`rclone config jsonconfig`" + ` – Created a new remote type X with parameters Y
 `,
 	Run: func(command *cobra.Command, args []string) {
 		cmd.CheckArgs(0, 4, command, args)
