a1d942e5c35f1ff31169e7fbd45bf89ca4e228d4
Nick Craig-Wood <nick@craig-wood.com>
2017-10-01 16:41:23 +0100

    pcloud: make compile with go1.6

--- pcloud/api/types.go
+++ pcloud/api/types.go
@@ -135,7 +135,7 @@ func (g *GetFileLinkResult) IsValid() bool {
 	if len(g.Hosts) == 0 {
 		return false
 	}
-	return time.Until(time.Time(g.Expires)) > 30*time.Second
+	return time.Time(g.Expires).Sub(time.Now()) > 30*time.Second
 }
 
 // URL returns a URL from the Path and Hosts.  Check with IsValid
