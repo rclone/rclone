085c690798cf618afd4aa89a45359311f18cdcc1
Nick Craig-Wood <nick@craig-wood.com>
2017-11-19 14:19:19 +0000

    build: add in 64bit path for WinFSP headers

--- bin/win-build.bat
+++ bin/win-build.bat
@@ -2,4 +2,4 @@
 echo Setting environment variables for mingw+WinFsp compile
 set GOPATH=X:\go
 set PATH=C:\Program Files\mingw-w64\i686-7.1.0-win32-dwarf-rt_v5-rev0\mingw32\bin;%PATH%
-set CPATH=C:\Program Files\WinFsp\inc\fuse
+set CPATH=C:\Program Files\WinFsp\inc\fuse;C:\Program Files (x86)\WinFsp\inc\fuse
