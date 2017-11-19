2b666187a6add5f652d594d9d59c2cb4c73a24fd
Nick Craig-Wood <nick@craig-wood.com>
2017-11-19 14:18:57 +0000

    cmount: disable tests on windows + race detector
    These either hang or produce incorrect results for reasons I haven't
    worked out yet.

--- cmd/cmount/mount_test.go
+++ cmd/cmount/mount_test.go
@@ -1,6 +1,10 @@
 // +build cmount
 // +build cgo
 // +build linux darwin freebsd windows
+// +build !race !windows
+
+// FIXME this doesn't work with the race detector under Windows either
+// hanging or producing lots of differences.
 
 package cmount
 
