2a6e18ec99ffc5079b9d8060c55e98b13e083909
Jody Frankowski <jody.frankowski@gmail.com>
2018-01-18 20:16:21 +0100

    Clean up mount.go and vfs/help.go docs
    * Title cleanups
    * Typos
    * `rclone mount vs rclone sync/copy` update with `File Caching`

--- vfs/help.go
+++ vfs/help.go
@@ -3,7 +3,7 @@ package vfs
 // Help contains text describing file and directory caching to add to
 // the command help.
 var Help = `
-### Directory Cache ###
+### Directory Cache
 
 Using the ` + "`--dir-cache-time`" + ` flag, you can set how long a
 directory should be considered up to date and not refreshed from the
@@ -18,12 +18,12 @@ like this:
 
     kill -SIGHUP $(pidof rclone)
 
-### File Caching ###
+### File Caching
 
 **NB** File caching is **EXPERIMENTAL** - use with care!
 
 These flags control the VFS file caching options.  The VFS layer is
-used by rclone mount to make a cloud storage systm work more like a
+used by rclone mount to make a cloud storage system work more like a
 normal file system.
 
 You'll need to enable VFS caching if you want, for example, to read
@@ -51,7 +51,7 @@ closed so if rclone is quit or dies with open files then these won't
 get written back to the remote.  However they will still be in the on
 disk cache.
 
-#### --vfs-cache-mode off ####
+#### --vfs-cache-mode off
 
 In this mode the cache will read directly from the remote and write
 directly to the remote without caching anything on disk.
@@ -66,7 +66,7 @@ This will mean some operations are not possible
   * Open modes O_APPEND, O_TRUNC are ignored
   * If an upload fails it can't be retried
 
-#### --vfs-cache-mode minimal ####
+#### --vfs-cache-mode minimal
 
 This is very similar to "off" except that files opened for read AND
 write will be buffered to disks.  This means that files opened for
@@ -79,7 +79,7 @@ These operations are not possible
   * Files opened for write only will ignore O_APPEND, O_TRUNC
   * If an upload fails it can't be retried
 
-#### --vfs-cache-mode writes ####
+#### --vfs-cache-mode writes
 
 In this mode files opened for read only are still read directly from
 the remote, write only and read/write files are buffered to disk
@@ -89,14 +89,14 @@ This mode should support all normal file system operations.
 
 If an upload fails it will be retried up to --low-level-retries times.
 
-#### --vfs-cache-mode full ####
+#### --vfs-cache-mode full
 
 In this mode all reads and writes are buffered to and from disk.  When
 a file is opened for read it will be downloaded in its entirety first.
 
 This may be appropriate for your needs, or you may prefer to look at
 the cache backend which does a much more sophisticated job of caching,
-including caching directory heirachies and chunks of files.q
+including caching directory hierarchies and chunks of files.
 
 In this mode, unlike the others, when a file is written to the disk,
 it will be kept on the disk after it is written to the remote.  It
