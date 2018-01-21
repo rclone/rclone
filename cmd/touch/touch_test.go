dec9ed39121a11183917823f6eb23556a662ba20
kubatasiemski <tasiemski@gmail.com>
2018-01-21 22:36:15 +0100

    cmd: rewrite touch tests #1934

--- cmd/touch/touch_test.go
+++ cmd/touch/touch_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/walk"
 	"github.com/ncw/rclone/fstest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,50 +12,111 @@ import (
 	_ "github.com/ncw/rclone/backend/local"
 )
 
-func TestTouch(t *testing.T) {
-	fstest.Initialise()
-	f, err := fs.NewFs("testfiles")
-	err = Touch(f, "newFile")
+var (
+	t1 = fstest.Time("2017-02-03T04:05:06.499999999Z")
+)
+
+// TestMain drives the tests
+func TestMain(m *testing.M) {
+	fstest.TestMain(m)
+}
+
+func TestTouchOneFile(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
+	err := Touch(r.Fremote, "newFile")
 	require.NoError(t, err)
-	file, errFile := f.NewObject("newFile")
-	require.NoError(t, errFile)
-	err = file.Remove()
+	_, err = r.Fremote.NewObject("newFile")
 	require.NoError(t, err)
+}
+
+func TestTouchWithNoCreateFlag(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
 
 	notCreateNewFile = true
-	err = Touch(f, "fileWithCflag")
+	err := Touch(r.Fremote, "newFile")
 	require.NoError(t, err)
-	file, errFile = f.NewObject("fileWithCflag")
-	require.Error(t, errFile)
+	_, err = r.Fremote.NewObject("newFile")
+	require.Error(t, err)
 	notCreateNewFile = false
+}
+
+func TestTouchWithTimestamp(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
 
 	timeAsArgument = "060102"
-	err = Touch(f, "oldFile")
+	err := Touch(r.Fremote, "oldFile")
 	require.NoError(t, err)
-	file, err = f.NewObject("oldFile")
+	file, err := r.Fremote.NewObject("oldFile")
 	require.NoError(t, err)
 	curretTime := time.Now()
-	require.NoError(t, err)
-	print(file.ModTime().Year() < curretTime.Year())
 	assert.Equal(t, true, file.ModTime().Year() < curretTime.Year())
-	err = file.Remove()
-	require.NoError(t, err)
+}
+
+func TestTouchWithLognerTimestamp(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
 
 	timeAsArgument = "2006-01-02T15:04:05"
-	err = Touch(f, "oldFile")
+	err := Touch(r.Fremote, "oldFile")
 	require.NoError(t, err)
-	file, err = f.NewObject("oldFile")
+	file, err := r.Fremote.NewObject("oldFile")
 	require.NoError(t, err)
+	curretTime := time.Now()
 	assert.Equal(t, true, file.ModTime().Year() < curretTime.Year())
+}
 
-	timeAsArgument = ""
-	err = Touch(f, "oldFile")
+func TestTouchUpdateTimestamp(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
+
+	file1 := r.WriteObject("a", "aaa", t1)
+	fstest.CheckItems(t, r.Fremote, file1)
+
+	file, err := r.Fremote.NewObject("a")
 	require.NoError(t, err)
-	file, err = f.NewObject("oldFile")
+	aSize := file.Size()
+	timeAsArgument = "121212"
+	err = Touch(r.Fremote, "a")
 	require.NoError(t, err)
-	timeBetween2007YearAndCurrent, errTime := time.Parse("060102", "121212")
-	require.NoError(t, errTime)
-	assert.Equal(t, true, file.ModTime().Year() > timeBetween2007YearAndCurrent.Year())
-	err = file.Remove()
+	file, err = r.Fremote.NewObject("a")
 	require.NoError(t, err)
+	assert.Equal(t, aSize, file.Size())
+	assert.Equal(t, 2012, file.ModTime().Year())
+}
+
+func TestTouchUpdateTimestampWithCFlag(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
+
+	file1 := r.WriteObject("a", "aaa", t1)
+	fstest.CheckItems(t, r.Fremote, file1)
+
+	notCreateNewFile = true
+	timeAsArgument = "121212"
+	err := Touch(r.Fremote, "a")
+	require.NoError(t, err)
+	file, err := r.Fremote.NewObject("a")
+	require.NoError(t, err)
+	assert.Equal(t, 2012, file.ModTime().Year())
+	notCreateNewFile = false
+}
+
+func TestTouchCreateMultipleDirAndFile(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
+
+	longPath := "a/b/c/d/e/f/g/h.txt"
+	err := Touch(r.Fremote, longPath)
+	require.NoError(t, err)
+	file, err := r.Fremote.NewObject(longPath)
+	require.NoError(t, err)
+	assert.Equal(t, longPath, file.Remote())
+
+	objs, dirs, err := walk.GetAll(r.Fremote, "", true, 1)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(dirs))
+	assert.Equal(t, 0, len(objs))
 }
