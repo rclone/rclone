229caa2df5abd0e5fa2355cdb6da436fdb4de260
kubatasiemski <tasiemski@gmail.com>
2018-01-31 00:33:41 +0100

    cmd: rewrite touch tests #1934

--- cmd/touch/touch_test.go
+++ cmd/touch/touch_test.go
@@ -4,9 +4,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ncw/rclone/fs/walk"
+	"github.com/ncw/rclone/fs"
 	"github.com/ncw/rclone/fstest"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	_ "github.com/ncw/rclone/backend/local"
@@ -16,6 +15,17 @@ var (
 	t1 = fstest.Time("2017-02-03T04:05:06.499999999Z")
 )
 
+func checkFile(t *testing.T, r fs.Fs, path string, content string) {
+	layout := defaultLayout
+	if len(timeAsArgument) == len(layoutDateWithTime) {
+		layout = layoutDateWithTime
+	}
+	timeAtrFromFlags, err := time.Parse(layout, timeAsArgument)
+	require.NoError(t, err)
+	file1 := fstest.NewItem(path, content, timeAtrFromFlags)
+	fstest.CheckItems(t, r, file1)
+}
+
 // TestMain drives the tests
 func TestMain(m *testing.M) {
 	fstest.TestMain(m)
@@ -24,6 +34,7 @@ func TestMain(m *testing.M) {
 func TestTouchOneFile(t *testing.T) {
 	r := fstest.NewRun(t)
 	defer r.Finalise()
+
 	err := Touch(r.Fremote, "newFile")
 	require.NoError(t, err)
 	_, err = r.Fremote.NewObject("newFile")
@@ -47,12 +58,10 @@ func TestTouchWithTimestamp(t *testing.T) {
 	defer r.Finalise()
 
 	timeAsArgument = "060102"
-	err := Touch(r.Fremote, "oldFile")
+	srcFileName := "oldFile"
+	err := Touch(r.Fremote, srcFileName)
 	require.NoError(t, err)
-	file, err := r.Fremote.NewObject("oldFile")
-	require.NoError(t, err)
-	curretTime := time.Now()
-	assert.Equal(t, true, file.ModTime().Year() < curretTime.Year())
+	checkFile(t, r.Fremote, srcFileName, "")
 }
 
 func TestTouchWithLognerTimestamp(t *testing.T) {
@@ -60,47 +69,41 @@ func TestTouchWithLognerTimestamp(t *testing.T) {
 	defer r.Finalise()
 
 	timeAsArgument = "2006-01-02T15:04:05"
-	err := Touch(r.Fremote, "oldFile")
+	srcFileName := "oldFile"
+	err := Touch(r.Fremote, srcFileName)
 	require.NoError(t, err)
-	file, err := r.Fremote.NewObject("oldFile")
-	require.NoError(t, err)
-	curretTime := time.Now()
-	assert.Equal(t, true, file.ModTime().Year() < curretTime.Year())
+	checkFile(t, r.Fremote, srcFileName, "")
 }
 
 func TestTouchUpdateTimestamp(t *testing.T) {
 	r := fstest.NewRun(t)
 	defer r.Finalise()
 
-	file1 := r.WriteObject("a", "aaa", t1)
+	srcFileName := "a"
+	content := "aaa"
+	file1 := r.WriteObject(srcFileName, content, t1)
 	fstest.CheckItems(t, r.Fremote, file1)
 
-	file, err := r.Fremote.NewObject("a")
-	require.NoError(t, err)
-	aSize := file.Size()
 	timeAsArgument = "121212"
-	err = Touch(r.Fremote, "a")
+	err := Touch(r.Fremote, "a")
 	require.NoError(t, err)
-	file, err = r.Fremote.NewObject("a")
-	require.NoError(t, err)
-	assert.Equal(t, aSize, file.Size())
-	assert.Equal(t, 2012, file.ModTime().Year())
+	checkFile(t, r.Fremote, srcFileName, content)
 }
 
 func TestTouchUpdateTimestampWithCFlag(t *testing.T) {
 	r := fstest.NewRun(t)
 	defer r.Finalise()
 
-	file1 := r.WriteObject("a", "aaa", t1)
+	srcFileName := "a"
+	content := "aaa"
+	file1 := r.WriteObject(srcFileName, content, t1)
 	fstest.CheckItems(t, r.Fremote, file1)
 
 	notCreateNewFile = true
 	timeAsArgument = "121212"
 	err := Touch(r.Fremote, "a")
 	require.NoError(t, err)
-	file, err := r.Fremote.NewObject("a")
-	require.NoError(t, err)
-	assert.Equal(t, 2012, file.ModTime().Year())
+	checkFile(t, r.Fremote, srcFileName, content)
 	notCreateNewFile = false
 }
 
@@ -108,15 +111,9 @@ func TestTouchCreateMultipleDirAndFile(t *testing.T) {
 	r := fstest.NewRun(t)
 	defer r.Finalise()
 
-	longPath := "a/b/c/d/e/f/g/h.txt"
+	longPath := "a/b/c.txt"
 	err := Touch(r.Fremote, longPath)
 	require.NoError(t, err)
-	file, err := r.Fremote.NewObject(longPath)
-	require.NoError(t, err)
-	assert.Equal(t, longPath, file.Remote())
-
-	objs, dirs, err := walk.GetAll(r.Fremote, "", true, 1)
-	require.NoError(t, err)
-	assert.Equal(t, 1, len(dirs))
-	assert.Equal(t, 0, len(objs))
+	file1 := fstest.NewItem("a/b/c.txt", "", t1)
+	fstest.CheckListingWithPrecision(t, r.Fremote, []fstest.Item{file1}, []string{"a", "a/b"}, fs.ModTimeNotSupported)
 }
