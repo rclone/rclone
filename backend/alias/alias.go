1889ea3a39bb15f1bb7a68c7fcb7e358c797127f
Fabian Möller <fabianm88@gmail.com>
2018-02-04 14:21:59 +0100

    alias: prevent use of empty remote

--- backend/alias/alias.go
+++ backend/alias/alias.go
@@ -29,6 +29,9 @@ func init() {
 // The returned Fs is the actual Fs, referenced by remote in the config
 func NewFs(name, root string) (fs.Fs, error) {
 	remote := config.FileGet(name, "remote")
+	if remote == "" {
+		return nil, errors.New("alias can't point to an empty remote - check the value of the remote setting")
+	}
 	if strings.HasPrefix(remote, name+":") {
 		return nil, errors.New("can't point alias remote at itself - check the value of the remote setting")
 	}
--- docs/content/alias.md
+++ docs/content/alias.md
@@ -22,6 +22,8 @@ is exactly the same as invoking `rclone mkdir mydrive:private/backup/desktop`.
 There will be no special handling of paths containing `..` segments.
 Invoking `rclone mkdir backup:../desktop` is exactly the same as invoking
 `rclone mkdir mydrive:private/backup/../desktop`.
+The empty path is not allowed as a remote. To alias the current directory
+use `.` instead.
 
 Here is an example of how to make a alias called `remote` for local folder.
 First run:
