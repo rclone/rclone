04a0a7406bbccd5f5f2c5b0e257001535cffde2b
Nick Craig-Wood <nick@craig-wood.com>
2018-02-09 07:57:50 +0000

    vfs: downgrade "poll-interval is not supported" message to Info
    ...to save confusion as it isn't very important

--- vfs/vfs.go
+++ vfs/vfs.go
@@ -220,7 +220,7 @@ func New(f fs.Fs, opt *Options) *VFS {
 		if do := vfs.f.Features().DirChangeNotify; do != nil {
 			do(vfs.root.ForgetPath, vfs.Opt.PollInterval)
 		} else {
-			fs.Logf(f, "poll-interval is not supported by this remote")
+			fs.Infof(f, "poll-interval is not supported by this remote")
 		}
 	}
 
