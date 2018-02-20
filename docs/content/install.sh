9d0a29eda94f2a8707d90de9f3a27d0e92a1601f
Daniel Loader <spicypixel@gmail.com>
2018-02-20 10:00:41 +0000

    Updated variable to check version post install

--- docs/content/install.sh
+++ docs/content/install.sh
@@ -170,6 +170,10 @@ case $OS in
     exit 2
 esac
 
-printf '\nrclone ${version} has successfully installed.'
+
+#update version variable post install
+version=`rclone --version 2>>errors | head -n 1`
+
+printf "\n${version} has successfully installed."
 printf '\nNow run "rclone config" for setup. Check https://rclone.org/docs/ for more details.\n\n'
 exit 0
