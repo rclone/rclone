f78829c95aef31e2b00f70c63d55c0105a6d4cac
Nick Craig-Wood <nick@craig-wood.com>
2018-02-28 21:27:34 +0000

    local: Downgrade "invalid cross-device link: trying copy" to debug - Fixes #1875

--- backend/local/local.go
+++ backend/local/local.go
@@ -491,7 +491,7 @@ func (f *Fs) Move(src fs.Object, remote string) (fs.Object, error) {
 	} else if err != nil {
 		// not quite clear, but probably trying to move a file across file system
 		// boundaries. Copying might still work.
-		fs.Errorf(src, "Can't move: %v: trying copy", err)
+		fs.Debugf(src, "Can't move: %v: trying copy", err)
 		return nil, fs.ErrorCantMove
 	}
 
@@ -545,7 +545,7 @@ func (f *Fs) DirMove(src fs.Fs, srcRemote, dstRemote string) error {
 	} else if err != nil {
 		// not quite clear, but probably trying to move directory across file system
 		// boundaries. Copying might still work.
-		fs.Errorf(src, "Can't move dir: %v: trying copy", err)
+		fs.Debugf(src, "Can't move dir: %v: trying copy", err)
 		return fs.ErrorCantDirMove
 	}
 	return nil
