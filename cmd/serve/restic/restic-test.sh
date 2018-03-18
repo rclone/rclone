1afac32d806925465909770a168886e8acd7d762
Nick Craig-Wood <nick@craig-wood.com>
2018-03-18 19:15:39 +0000

    serve restic: script for running integration test against all remotes

---
+++ cmd/serve/restic/restic-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Test all the remotes against restic integration test
+# Run with: screen -S restic-test -L ./restic-test.sh
+
+remotes="
+TestAzureBlob:
+TestB2:
+TestBox:
+TestCache:
+TestCryptDrive:
+TestCryptSwift:
+TestDrive:
+TestDropbox:
+TestFTP:
+TestGoogleCloudStorage:
+TestHubic:
+TestOneDrive:
+TestPcloud:
+TestQingStor:
+TestS3:
+TestSftp:
+TestSwift:
+TestWebdav:
+TestYandex:
+"
+
+# TestOss:
+# TestMega:
+
+for remote in $remotes; do
+    echo `date -Is` $remote starting
+    go test -remote $remote -v -timeout 30m 2>&1 | tee restic-test.$remote.log
+    echo `date -Is` $remote ending
+done
