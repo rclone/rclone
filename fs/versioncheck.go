ec9c74fee66393cef60f794e3a17076a2f6e5997
Nick Craig-Wood <nick@craig-wood.com>
2018-04-06 19:04:31 +0100

    Require go1.7 for compilation #2154
      * Update the travis tests to exclude go1.6
      * Update the compile check to require go1.7+
      * Update misc go1.6 workarounds marked in the source

--- fs/versioncheck.go
+++ fs/versioncheck.go
@@ -1,7 +1,7 @@
-//+build !go1.6
+//+build !go1.7
 
 package fs
 
-// Upgrade to Go version 1.6 to compile rclone - latest stable go
+// Upgrade to Go version 1.7 to compile rclone - latest stable go
 // compiler recommended.
-func init() { Go_version_1_6_required_for_compilation() }
+func init() { Go_version_1_7_required_for_compilation() }
