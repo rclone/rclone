be22735609356749c6da6ea55553bb65a4740f06
Nick Craig-Wood <nick@craig-wood.com>
2018-05-02 17:01:39 +0100

    fs/accounting: fix deadlock on GetBytes
    A deadlock could occur since we have now put a mutex on GetBytes from
    StatsInfo.String (s.mu) - progress (acc.statmu) and read (acc.statmu)
    - GetBytes (s.mu).
    
    Fix this by giving stringSet its own locking and excluding the call
    which caused the deadlock from the mutex in StatsInfo.String.

--- fs/accounting/stringset.go
+++ fs/accounting/stringset.go
@@ -3,15 +3,49 @@ package accounting
 import (
 	"sort"
 	"strings"
+	"sync"
 )
 
 // stringSet holds a set of strings
-type stringSet map[string]struct{}
+type stringSet struct {
+	mu    sync.RWMutex
+	items map[string]struct{}
+}
+
+// newStringSet creates a new empty string set of capacity size
+func newStringSet(size int) *stringSet {
+	return &stringSet{
+		items: make(map[string]struct{}, size),
+	}
+}
+
+// add adds remote to the set
+func (ss *stringSet) add(remote string) {
+	ss.mu.Lock()
+	ss.items[remote] = struct{}{}
+	ss.mu.Unlock()
+}
+
+// del removes remote from the set
+func (ss *stringSet) del(remote string) {
+	ss.mu.Lock()
+	delete(ss.items, remote)
+	ss.mu.Unlock()
+}
+
+// empty returns whether the set has any items
+func (ss *stringSet) empty() bool {
+	ss.mu.RLock()
+	defer ss.mu.RUnlock()
+	return len(ss.items) == 0
+}
 
 // Strings returns all the strings in the stringSet
-func (ss stringSet) Strings() []string {
-	strings := make([]string, 0, len(ss))
-	for name := range ss {
+func (ss *stringSet) Strings() []string {
+	ss.mu.RLock()
+	defer ss.mu.RUnlock()
+	strings := make([]string, 0, len(ss.items))
+	for name := range ss.items {
 		var out string
 		if acc := Stats.inProgress.get(name); acc != nil {
 			out = acc.String()
@@ -26,6 +60,6 @@ func (ss stringSet) Strings() []string {
 }
 
 // String returns all the file names in the stringSet joined by newline
-func (ss stringSet) String() string {
+func (ss *stringSet) String() string {
 	return strings.Join(ss.Strings(), "\n")
 }
