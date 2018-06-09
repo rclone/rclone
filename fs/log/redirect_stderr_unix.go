ce109ed9c0753e3b684a00db692228819f876e88
Filip Bartodziej <filipbartodziej@gmail.com>
2018-06-10 00:27:42 +0200

    log: password prompt output fixed for unix - partially fixes #2220

--- fs/log/redirect_stderr_unix.go
+++ fs/log/redirect_stderr_unix.go
@@ -8,12 +8,19 @@ import (
 	"log"
 	"os"
 
+	"github.com/ncw/rclone/fs/config"
+
 	"golang.org/x/sys/unix"
 )
 
 // redirectStderr to the file passed in
 func redirectStderr(f *os.File) {
-	err := unix.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+	passPromptFd, err := unix.Dup(int(os.Stderr.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to duplicate stderr: %v", err)
+	}
+	config.PasswordPromptOutput = os.NewFile(uintptr(passPromptFd), "passPrompt")
+	err = unix.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
 	if err != nil {
 		log.Fatalf("Failed to redirect stderr to file: %v", err)
 	}
