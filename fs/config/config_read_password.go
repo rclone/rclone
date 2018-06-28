65fdb3e8821ec7ddd6077578294c1184279c2435
Nick Craig-Wood <nick@craig-wood.com>
2018-06-28 11:54:15 +0100

    config: fix error reading password from piped input - fixes #1308

--- fs/config/config_read_password.go
+++ fs/config/config_read_password.go
@@ -16,7 +16,11 @@ import (
 
 // ReadPassword reads a password without echoing it to the terminal.
 func ReadPassword() string {
-	line, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	stdin := int(os.Stdin.Fd())
+	if !terminal.IsTerminal(stdin) {
+		return ReadLine()
+	}
+	line, err := terminal.ReadPassword(stdin)
 	_, _ = fmt.Fprintln(os.Stderr)
 	if err != nil {
 		log.Fatalf("Failed to read password: %v", err)
