7712a0e111c7a37571f3fb796fe29dd9f44e5cfa
Nick Craig-Wood <nick@craig-wood.com>
2018-08-20 10:44:54 +0100

    fs/asyncreader: skip some tests to work around race detector bug
    The race detector currently detects a race with len(chan) against
    close(chan).
    
    See: https://github.com/golang/go/issues/27070
    
    Skip the tests which trip this bug under the race detector.

---
+++ lib/israce/norace.go
@@ -0,0 +1,9 @@
+// +build !race
+
+// Package israce reports if the Go race detector is enabled.
+//
+// From https://stackoverflow.com/questions/44944959/how-can-i-check-if-the-race-detector-is-enabled-at-runtime
+package israce
+
+// Enabled reports if the race detector is enabled.
+const Enabled = false
