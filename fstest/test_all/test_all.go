c56ecc866b634b0ea44dc24d3862da39f0e2eb7b
Nick Craig-Wood <nick@craig-wood.com>
2018-09-29 14:48:29 +0100

    fstest/test_all: rework integration tests to improve output
    - Make integration tests use a config file
    - Output individual logs for each test
    - Make HTML report and open browser
    - Optionally email and upload results

--- fstest/test_all/test_all.go
+++ fstest/test_all/test_all.go
@@ -4,24 +4,22 @@
 // See the `test` target in the Makefile.
 package main
 
+/* FIXME
+
+Make TesTrun have a []string of flags to try - that then makes it generic
+
+*/
+
 import (
 	"flag"
-	"go/build"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 	"regexp"
-	"runtime"
 	"strings"
 	"time"
 
 	_ "github.com/ncw/rclone/backend/all" // import all fs
-	"github.com/ncw/rclone/fs"
-	"github.com/ncw/rclone/fs/config"
-	"github.com/ncw/rclone/fs/list"
-	"github.com/ncw/rclone/fs/operations"
-	"github.com/ncw/rclone/fstest"
 )
 
 type remoteConfig struct {
@@ -31,217 +29,22 @@ type remoteConfig struct {
 }
 
 var (
-	remotes = []remoteConfig{
-		// {
-		// 	Name:     "TestAmazonCloudDrive:",
-		// 	SubDir:   false,
-		// 	FastList: false,
-		// },
-		{
-			Name:     "TestB2:",
-			SubDir:   true,
-			FastList: true,
-		},
-		{
-			Name:     "TestCryptDrive:",
-			SubDir:   false,
-			FastList: true,
-		},
-		{
-			Name:     "TestCryptSwift:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestDrive:",
-			SubDir:   false,
-			FastList: true,
-		},
-		{
-			Name:     "TestDropbox:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestGoogleCloudStorage:",
-			SubDir:   true,
-			FastList: true,
-		},
-		{
-			Name:     "TestHubic:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestJottacloud:",
-			SubDir:   false,
-			FastList: true,
-		},
-		{
-			Name:     "TestOneDrive:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestS3:",
-			SubDir:   true,
-			FastList: true,
-		},
-		{
-			Name:     "TestSftp:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestSwift:",
-			SubDir:   true,
-			FastList: true,
-		},
-		{
-			Name:     "TestYandex:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestFTP:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestBox:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestQingStor:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestAzureBlob:",
-			SubDir:   true,
-			FastList: true,
-		},
-		{
-			Name:     "TestPcloud:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestWebdav:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestCache:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestMega:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestOpenDrive:",
-			SubDir:   false,
-			FastList: false,
-		},
-		{
-			Name:     "TestUnion:",
-			SubDir:   false,
-			FastList: false,
-		},
-	}
 	// Flags
-	maxTries = flag.Int("maxtries", 5, "Number of times to try each test")
-	runTests = flag.String("remotes", "", "Comma separated list of remotes to test, eg 'TestSwift:,TestS3'")
-	clean    = flag.Bool("clean", false, "Instead of testing, clean all left over test directories")
-	runOnly  = flag.String("run", "", "Run only those tests matching the regexp supplied")
-	timeout  = flag.Duration("timeout", 30*time.Minute, "Maximum time to run each test for before giving up")
+	maxTries     = flag.Int("maxtries", 5, "Number of times to try each test")
+	testRemotes  = flag.String("remotes", "", "Comma separated list of remotes to test, eg 'TestSwift:,TestS3'")
+	testBackends = flag.String("backends", "", "Comma separated list of backends to test, eg 's3,googlecloudstorage")
+	testTests    = flag.String("tests", "", "Comma separated list of tests to test, eg 'fs/sync,fs/operations'")
+	clean        = flag.Bool("clean", false, "Instead of testing, clean all left over test directories")
+	runOnly      = flag.String("run", "", "Run only those tests matching the regexp supplied")
+	timeout      = flag.Duration("timeout", 30*time.Minute, "Maximum time to run each test for before giving up")
+	configFile   = flag.String("config", "fstest/test_all/config.yaml", "Path to config file")
+	outputDir    = flag.String("output", path.Join(os.TempDir(), "rclone-integration-tests"), "Place to store results")
+	emailReport  = flag.String("email", "", "Set to email the report to the address supplied")
+	dryRun       = flag.Bool("dry-run", false, "Print commands which would be executed only")
+	urlBase      = flag.String("url-base", "https://pub.rclone.org/integration-tests/", "Base for the online version")
+	uploadPath   = flag.String("upload", "", "Set this to an rclone path to upload the results here")
+	verbose      = flag.Bool("verbose", false, "Set to enable verbose logging in the tests")
 )
-
-// test holds info about a running test
-type test struct {
-	pkg         string
-	remote      string
-	subdir      bool
-	cmdLine     []string
-	cmdString   string
-	try         int
-	err         error
-	output      []byte
-	failedTests []string
-	runFlag     string
-}
-
-// newTest creates a new test
-func newTest(pkg, remote string, subdir bool, fastlist bool) *test {
-	binary := pkgBinary(pkg)
-	t := &test{
-		pkg:     pkg,
-		remote:  remote,
-		subdir:  subdir,
-		cmdLine: []string{binary, "-test.timeout", timeout.String(), "-remote", remote},
-		try:     1,
-	}
-	if *fstest.Verbose {
-		t.cmdLine = append(t.cmdLine, "-test.v")
-		fs.Config.LogLevel = fs.LogLevelDebug
-	}
-	if *runOnly != "" {
-		t.cmdLine = append(t.cmdLine, "-test.run", *runOnly)
-	}
-	if subdir {
-		t.cmdLine = append(t.cmdLine, "-subdir")
-	}
-	if fastlist {
-		t.cmdLine = append(t.cmdLine, "-fast-list")
-	}
-	t.cmdString = toShell(t.cmdLine)
-	return t
-}
-
-// dumpOutput prints the error output
-func (t *test) dumpOutput() {
-	log.Println("------------------------------------------------------------")
-	log.Printf("---- %q ----", t.cmdString)
-	log.Println(string(t.output))
-	log.Println("------------------------------------------------------------")
-}
-
-var failRe = regexp.MustCompile(`(?m)^--- FAIL: (Test\w*) \(`)
-
-// findFailures looks for all the tests which failed
-func (t *test) findFailures() {
-	oldFailedTests := t.failedTests
-	t.failedTests = nil
-	for _, matches := range failRe.FindAllSubmatch(t.output, -1) {
-		t.failedTests = append(t.failedTests, string(matches[1]))
-	}
-	if len(t.failedTests) != 0 {
-		t.runFlag = "^(" + strings.Join(t.failedTests, "|") + ")$"
-	} else {
-		t.runFlag = ""
-	}
-	if t.passed() && len(t.failedTests) != 0 {
-		log.Printf("%q - Expecting no errors but got: %v", t.cmdString, t.failedTests)
-		t.dumpOutput()
-	} else if !t.passed() && len(t.failedTests) == 0 {
-		log.Printf("%q - Expecting errors but got none: %v", t.cmdString, t.failedTests)
-		t.dumpOutput()
-		t.failedTests = oldFailedTests
-	}
-}
-
-// nextCmdLine returns the next command line
-func (t *test) nextCmdLine() []string {
-	cmdLine := t.cmdLine
-	if t.runFlag != "" {
-		cmdLine = append(cmdLine, "-test.run", t.runFlag)
-	}
-	return cmdLine
-}
 
 // if matches then is definitely OK in the shell
 var shellOK = regexp.MustCompile("^[A-Za-z0-9./_:-]+$")
@@ -261,181 +64,53 @@ func toShell(args []string) (result string) {
 	return result
 }
 
-// trial runs a single test
-func (t *test) trial() {
-	cmdLine := t.nextCmdLine()
-	cmdString := toShell(cmdLine)
-	log.Printf("%q - Starting (try %d/%d)", cmdString, t.try, *maxTries)
-	cmd := exec.Command(cmdLine[0], cmdLine[1:]...)
-	start := time.Now()
-	t.output, t.err = cmd.CombinedOutput()
-	duration := time.Since(start)
-	t.findFailures()
-	if t.passed() {
-		log.Printf("%q - Finished OK in %v (try %d/%d)", cmdString, duration, t.try, *maxTries)
-	} else {
-		log.Printf("%q - Finished ERROR in %v (try %d/%d): %v: Failed %v", cmdString, duration, t.try, *maxTries, t.err, t.failedTests)
-	}
-}
-
-// cleanFs runs a single clean fs for left over directories
-func (t *test) cleanFs() error {
-	f, err := fs.NewFs(t.remote)
-	if err != nil {
-		return err
-	}
-	entries, err := list.DirSorted(f, true, "")
-	if err != nil {
-		return err
-	}
-	return entries.ForDirError(func(dir fs.Directory) error {
-		remote := dir.Remote()
-		if fstest.MatchTestRemote.MatchString(remote) {
-			log.Printf("Purging %s%s", t.remote, remote)
-			dir, err := fs.NewFs(t.remote + remote)
-			if err != nil {
-				return err
-			}
-			return operations.Purge(dir, "")
-		}
-		return nil
-	})
-}
-
-// clean runs a single clean on a fs for left over directories
-func (t *test) clean() {
-	log.Printf("%q - Starting clean (try %d/%d)", t.remote, t.try, *maxTries)
-	start := time.Now()
-	t.err = t.cleanFs()
-	if t.err != nil {
-		log.Printf("%q - Failed to purge %v", t.remote, t.err)
-	}
-	duration := time.Since(start)
-	if t.passed() {
-		log.Printf("%q - Finished OK in %v (try %d/%d)", t.cmdString, duration, t.try, *maxTries)
-	} else {
-		log.Printf("%q - Finished ERROR in %v (try %d/%d): %v", t.cmdString, duration, t.try, *maxTries, t.err)
-	}
-}
-
-// passed returns true if the test passed
-func (t *test) passed() bool {
-	return t.err == nil
-}
-
-// run runs all the trials for this test
-func (t *test) run(result chan<- *test) {
-	for t.try = 1; t.try <= *maxTries; t.try++ {
-		if *clean {
-			if !t.subdir {
-				t.clean()
-			}
-		} else {
-			t.trial()
-		}
-		if t.passed() {
-			break
-		}
-	}
-	if !t.passed() {
-		t.dumpOutput()
-	}
-	result <- t
-}
-
-// GOPATH returns the current GOPATH
-func GOPATH() string {
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		gopath = build.Default.GOPATH
-	}
-	return gopath
-}
-
-// turn a package name into a binary name
-func pkgBinaryName(pkg string) string {
-	binary := path.Base(pkg) + ".test"
-	if runtime.GOOS == "windows" {
-		binary += ".exe"
-	}
-	return binary
-}
-
-// turn a package name into a binary path
-func pkgBinary(pkg string) string {
-	return path.Join(pkgPath(pkg), pkgBinaryName(pkg))
-}
-
-// returns the path to the package
-func pkgPath(pkg string) string {
-	return path.Join(GOPATH(), "src", pkg)
-}
-
-// cd into the package directory
-func pkgChdir(pkg string) {
-	err := os.Chdir(pkgPath(pkg))
-	if err != nil {
-		log.Fatalf("Failed to chdir to package %q: %v", pkg, err)
-	}
-}
-
-// makeTestBinary makes the binary we will run
-func makeTestBinary(pkg string) {
-	binaryName := pkgBinaryName(pkg)
-	log.Printf("%s: Making test binary %q", pkg, binaryName)
-	pkgChdir(pkg)
-	err := exec.Command("go", "test", "-c", "-o", binaryName).Run()
-	if err != nil {
-		log.Fatalf("Failed to make test binary: %v", err)
-	}
-	binary := pkgBinary(pkg)
-	if _, err := os.Stat(binary); err != nil {
-		log.Fatalf("Couldn't find test binary %q", binary)
-	}
-}
-
-// removeTestBinary removes the binary made in makeTestBinary
-func removeTestBinary(pkg string) {
-	binary := pkgBinary(pkg)
-	err := os.Remove(binary) // Delete the binary when finished
-	if err != nil {
-		log.Printf("Error removing test binary %q: %v", binary, err)
-	}
-}
-
 func main() {
 	flag.Parse()
-	packages := flag.Args()
-	log.Printf("Testing packages: %s", strings.Join(packages, ", "))
-	if *runTests != "" {
-		newRemotes := []remoteConfig{}
-		for _, name := range strings.Split(*runTests, ",") {
-			for i := range remotes {
-				if remotes[i].Name == name {
-					newRemotes = append(newRemotes, remotes[i])
-					goto found
-				}
-			}
-			log.Printf("Remote %q not found - inserting with default flags", name)
-			newRemotes = append(newRemotes, remoteConfig{Name: name})
-		found:
-		}
-		remotes = newRemotes
+	conf, err := NewConfig(*configFile)
+	if err != nil {
+		log.Println("test_all should be run from the root of the rclone source code")
+		log.Fatal(err)
 	}
+
+	// Filter selection
+	if *testRemotes != "" {
+		conf.filterBackendsByRemotes(strings.Split(*testRemotes, ","))
+	}
+	if *testBackends != "" {
+		conf.filterBackendsByBackends(strings.Split(*testBackends, ","))
+	}
+	if *testTests != "" {
+		conf.filterTests(strings.Split(*testTests, ","))
+	}
+
+	// Just clean the directories if required
+	if *clean {
+		err := cleanRemotes(conf.Remotes())
+		if err != nil {
+			log.Fatalf("Failed to clean: %v", err)
+		}
+		return
+	}
+
 	var names []string
-	for _, remote := range remotes {
-		names = append(names, remote.Name)
+	for _, remote := range conf.Backends {
+		names = append(names, remote.Remote)
 	}
 	log.Printf("Testing remotes: %s", strings.Join(names, ", "))
 
-	start := time.Now()
-	if *clean {
-		config.LoadConfig()
-		packages = []string{"clean"}
-	} else {
-		for _, pkg := range packages {
-			makeTestBinary(pkg)
-			defer removeTestBinary(pkg)
+	// Runs we will do for this test
+	runs := conf.MakeRuns()
+
+	// Create Report
+	report := NewReport()
+
+	// Make the test binaries, one per Path found in the tests
+	done := map[string]struct{}{}
+	for _, run := range runs {
+		if _, found := done[run.Path]; !found {
+			done[run.Path] = struct{}{}
+			run.MakeTestBinary()
+			defer run.RemoveTestBinary()
 		}
 	}
 
@@ -443,46 +118,26 @@ func main() {
 	_ = os.Setenv("RCLONE_CACHE_DB_WAIT_TIME", "30m")
 
 	// start the tests
-	results := make(chan *test, 8)
+	results := make(chan *Run, 8)
 	awaiting := 0
-	bools := []bool{false, true}
-	if *clean {
-		// Don't run -subdir and -fast-list if -clean
-		bools = bools[:1]
-	}
-	for _, pkg := range packages {
-		for _, remote := range remotes {
-			for _, subdir := range bools {
-				for _, fastlist := range bools {
-					if (!subdir || subdir && remote.SubDir) && (!fastlist || fastlist && remote.FastList) {
-						go newTest(pkg, remote.Name, subdir, fastlist).run(results)
-						awaiting++
-					}
-				}
-			}
-		}
+	for _, run := range runs {
+		go run.Run(report.LogDir, results)
+		awaiting++
 	}
 
 	// Wait for the tests to finish
-	var failed []*test
 	for ; awaiting > 0; awaiting-- {
 		t := <-results
-		if !t.passed() {
-			failed = append(failed, t)
-		}
+		report.RecordResult(t)
 	}
-	duration := time.Since(start)
 
-	// Summarise results
-	log.Printf("SUMMARY")
-	if len(failed) == 0 {
-		log.Printf("PASS: All tests finished OK in %v", duration)
-	} else {
-		log.Printf("FAIL: %d tests failed in %v", len(failed), duration)
-		for _, t := range failed {
-			log.Printf("  * %s", toShell(t.nextCmdLine()))
-			log.Printf("    * Failed tests: %v", t.failedTests)
-		}
+	// Log and exit
+	report.End()
+	report.LogSummary()
+	report.LogHTML()
+	report.EmailHTML()
+	report.Upload()
+	if !report.AllPassed() {
 		os.Exit(1)
 	}
 }
