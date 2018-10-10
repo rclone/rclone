cf236c6881adab599756c29794bfeefdc2db9879
teresy <hi.teresy@gmail.com>
2018-10-10 15:28:38 -0400

    Remove redundant nil checks

--- backend/yandex/api/client.go
+++ backend/yandex/api/client.go
@@ -67,10 +67,8 @@ var defaultErrorHandler ErrorHandler = func(resp *http.Response) error {
 func (HTTPRequest *HTTPRequest) run(client *Client) ([]byte, error) {
 	var err error
 	values := make(url.Values)
-	if HTTPRequest.Parameters != nil {
-		for k, v := range HTTPRequest.Parameters {
-			values.Set(k, fmt.Sprintf("%v", v))
-		}
+	for k, v := range HTTPRequest.Parameters {
+		values.Set(k, fmt.Sprintf("%v", v))
 	}
 
 	var req *http.Request
