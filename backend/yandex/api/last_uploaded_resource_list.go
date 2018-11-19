95cde131bd6ea1fbb24e7bb0e4be997b883a7519
Sebastian Bünger <buengese@gmail.com>
2018-11-19 18:27:00 +0100

    yandex: complete rewrite Get rid of the api client and use rest/pacer for all API calls Add Copy, Move, DirMove, PublicLink, About optional interfaces Improve general error handling Remove ListR for now due to inconsitent behaviour fixes #2586, progress on #2740 and #2178

--- backend/yandex/api/last_uploaded_resource_list.go
+++
@@ -1,7 +0,0 @@
-package src
-
-// LastUploadedResourceListResponse struct
-type LastUploadedResourceListResponse struct {
-	Items []ResourceInfoResponse `json:"items"`
-	Limit *uint64                `json:"limit"`
-}
