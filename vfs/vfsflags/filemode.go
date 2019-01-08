554ee0d96351f343adf93e3408940f7ec85664e9
Nick Craig-Wood <nick@craig-wood.com>
2019-01-08 10:52:14 +0000

    vfs: add --dir-perms and --file-perms flags - fixes #2897
    This allows files to be shown with the execute bit which allows
    binaries to be run under Windows and Linux.

---
+++ vfs/vfsflags/filemode.go
@@ -0,0 +1,34 @@
+package vfsflags
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+// FileMode is a command line friendly os.FileMode
+type FileMode struct {
+	Mode *os.FileMode
+}
+
+// String turns FileMode into a string
+func (x *FileMode) String() string {
+	return fmt.Sprintf("0%3o", *x.Mode)
+}
+
+// Set a FileMode
+func (x *FileMode) Set(s string) error {
+	i, err := strconv.ParseInt(s, 8, 64)
+	if err != nil {
+		return errors.Wrap(err, "Bad FileMode - must be octal digits")
+	}
+	*x.Mode = (os.FileMode)(i)
+	return nil
+}
+
+// Type of the value
+func (x *FileMode) Type() string {
+	return "int"
+}
