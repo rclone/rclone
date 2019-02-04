7d5d6c041f8dc8892924418d55b5ab8ea637d931
Nick Craig-Wood <nick@craig-wood.com>
2019-02-04 17:33:15 +0000

    vendor: update github.com/t3rm1n4l/go-mega to fix v2 account login
    Fixes #2771

--- vendor/github.com/t3rm1n4l/go-mega/messages.go
+++ vendor/github.com/t3rm1n4l/go-mega/messages.go
@@ -2,16 +2,32 @@ package mega
 
 import "encoding/json"
 
+type PreloginMsg struct {
+	Cmd  string `json:"a"`
+	User string `json:"user"`
+}
+
+type PreloginResp struct {
+	Version int    `json:"v"`
+	Salt    string `json:"s"`
+}
+
 type LoginMsg struct {
-	Cmd    string `json:"a"`
-	User   string `json:"user"`
-	Handle string `json:"uh"`
+	Cmd        string `json:"a"`
+	User       string `json:"user"`
+	Handle     string `json:"uh"`
+	SessionKey string `json:"sek,omitempty"`
+	Si         string `json:"si,omitempty"`
+	Mfa        string `json:"mfa,omitempty"`
 }
 
 type LoginResp struct {
-	Csid  string `json:"csid"`
-	Privk string `json:"privk"`
-	Key   string `json:"k"`
+	Csid       string `json:"csid"`
+	Privk      string `json:"privk"`
+	Key        string `json:"k"`
+	Ach        int    `json:"ach"`
+	SessionKey string `json:"sek"`
+	U          string `json:"u"`
 }
 
 type UserMsg struct {
