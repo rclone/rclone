475cead22898bbbb0b82ee98216a9d2dd2c49d99
Animosity022 <earl.texter@gmail.com>
2019-04-25 07:32:36 -0400

    docs: Typo fixes with "a existing"
    Fixed a typo with "a existing" to "an existing"

--- backend/alias/alias.go
+++ backend/alias/alias.go
@@ -14,7 +14,7 @@ import (
 func init() {
 	fsi := &fs.RegInfo{
 		Name:        "alias",
-		Description: "Alias for a existing remote",
+		Description: "Alias for an existing remote",
 		NewFs:       NewFs,
 		Options: []fs.Option{{
 			Name:     "remote",
