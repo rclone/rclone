a8c40cb09641748610c6fccf54f41c9e0ec41321
Nick Craig-Wood <nick@craig-wood.com>
2019-05-10 19:00:18 +0100

    serve sftp: serve an rclone remote over SFTP

---
+++ cmd/serve/sftp/handler.go
@@ -0,0 +1,154 @@
+// +build !plan9
+
+package sftp
+
+import (
+	"io"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/vfs"
+	"github.com/pkg/sftp"
+)
+
+// vfsHandler converts the VFS to be served by SFTP
+type vfsHandler struct {
+	*vfs.VFS
+}
+
+// vfsHandler returns a Handlers object with the test handlers.
+func newVFSHandler(vfs *vfs.VFS) (sftp.Handlers, error) {
+	v := vfsHandler{VFS: vfs}
+	return sftp.Handlers{
+		FileGet:  v,
+		FilePut:  v,
+		FileCmd:  v,
+		FileList: v,
+	}, nil
+}
+
+func (v vfsHandler) Fileread(r *sftp.Request) (io.ReaderAt, error) {
+	file, err := v.OpenFile(r.Filepath, os.O_RDONLY, 0777)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+func (v vfsHandler) Filewrite(r *sftp.Request) (io.WriterAt, error) {
+	file, err := v.OpenFile(r.Filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+func (v vfsHandler) Filecmd(r *sftp.Request) error {
+	switch r.Method {
+	case "Setstat":
+		node, err := v.Stat(r.Filepath)
+		if err != nil {
+			return err
+		}
+		attr := r.Attributes()
+		if attr.Mtime != 0 {
+			modTime := time.Unix(int64(attr.Mtime), 0)
+			err := node.SetModTime(modTime)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	case "Rename":
+		err := v.Rename(r.Filepath, r.Target)
+		if err != nil {
+			return err
+		}
+	case "Rmdir", "Remove":
+		node, err := v.Stat(r.Filepath)
+		if err != nil {
+			return err
+		}
+		err = node.Remove()
+		if err != nil {
+			return err
+		}
+	case "Mkdir":
+		dir, leaf, err := v.StatParent(r.Filepath)
+		if err != nil {
+			return err
+		}
+		_, err = dir.Mkdir(leaf)
+		if err != nil {
+			return err
+		}
+	case "Symlink":
+		// FIXME
+		// _, err := v.fetch(r.Filepath)
+		// if err != nil {
+		// 	return err
+		// }
+		// link := newMemFile(r.Target, false)
+		// link.symlink = r.Filepath
+		// v.files[r.Target] = link
+	}
+	return nil
+}
+
+type listerat []os.FileInfo
+
+// Modeled after strings.Reader's ReadAt() implementation
+func (f listerat) ListAt(ls []os.FileInfo, offset int64) (int, error) {
+	var n int
+	if offset >= int64(len(f)) {
+		return 0, io.EOF
+	}
+	n = copy(ls, f[offset:])
+	if n < len(ls) {
+		return n, io.EOF
+	}
+	return n, nil
+}
+
+func (v vfsHandler) Filelist(r *sftp.Request) (l sftp.ListerAt, err error) {
+	var node vfs.Node
+	var handle vfs.Handle
+	switch r.Method {
+	case "List":
+		node, err = v.Stat(r.Filepath)
+		if err != nil {
+			return nil, err
+		}
+		if !node.IsDir() {
+			return nil, syscall.ENOTDIR
+		}
+		handle, err = node.Open(os.O_RDONLY)
+		if err != nil {
+			return nil, err
+		}
+		defer fs.CheckClose(handle, &err)
+		fis, err := handle.Readdir(-1)
+		if err != nil {
+			return nil, err
+		}
+		return listerat(fis), nil
+	case "Stat":
+		node, err = v.Stat(r.Filepath)
+		if err != nil {
+			return nil, err
+		}
+		return listerat([]os.FileInfo{node}), nil
+	case "Readlink":
+		// FIXME
+		// if file.symlink != "" {
+		// 	file, err = v.fetch(file.symlink)
+		// 	if err != nil {
+		// 		return nil, err
+		// 	}
+		// }
+		// return listerat([]os.FileInfo{file}), nil
+	}
+	return nil, nil
+}
---
+++ cmd/serve/sftp/sftp_test.go
@@ -0,0 +1,94 @@
+// Serve sftp tests set up a server and run the integration tests
+// for the sftp remote against it.
+//
+// We skip tests on platforms with troublesome character mappings
+
+//+build !windows,!darwin,!plan9
+
+package sftp
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	_ "github.com/ncw/rclone/backend/local"
+	"github.com/ncw/rclone/fs/config/obscure"
+	"github.com/ncw/rclone/fstest"
+	"github.com/pkg/sftp"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testBindAddress = "localhost:0"
+	testUser        = "testuser"
+	testPass        = "testpass"
+)
+
+// check interfaces
+var (
+	_ sftp.FileReader = vfsHandler{}
+	_ sftp.FileWriter = vfsHandler{}
+	_ sftp.FileCmder  = vfsHandler{}
+	_ sftp.FileLister = vfsHandler{}
+)
+
+// TestSftp runs the sftp server then runs the unit tests for the
+// sftp remote against it.
+func TestSftp(t *testing.T) {
+	fstest.Initialise()
+
+	fremote, _, clean, err := fstest.RandomRemote(*fstest.RemoteName, *fstest.SubDir)
+	assert.NoError(t, err)
+	defer clean()
+
+	err = fremote.Mkdir("")
+	assert.NoError(t, err)
+
+	opt := DefaultOpt
+	opt.ListenAddr = testBindAddress
+	opt.User = testUser
+	opt.Pass = testPass
+
+	// Start the server
+	w := newServer(fremote, &opt)
+	assert.NoError(t, w.serve())
+	defer func() {
+		w.Close()
+		w.Wait()
+	}()
+
+	// Change directory to run the tests
+	err = os.Chdir("../../../backend/sftp")
+	assert.NoError(t, err, "failed to cd to sftp backend")
+
+	// Run the sftp tests with an on the fly remote
+	args := []string{"test"}
+	if testing.Verbose() {
+		args = append(args, "-v")
+	}
+	if *fstest.Verbose {
+		args = append(args, "-verbose")
+	}
+	args = append(args, "-remote", "sftptest:")
+	cmd := exec.Command("go", args...)
+	addr := w.Addr()
+	colon := strings.LastIndex(addr, ":")
+	if colon < 0 {
+		panic("need a : in the address: " + addr)
+	}
+	host, port := addr[:colon], addr[colon+1:]
+	cmd.Env = append(os.Environ(),
+		"RCLONE_CONFIG_SFTPTEST_TYPE=sftp",
+		"RCLONE_CONFIG_SFTPTEST_HOST="+host,
+		"RCLONE_CONFIG_SFTPTEST_PORT="+port,
+		"RCLONE_CONFIG_SFTPTEST_USER="+testUser,
+		"RCLONE_CONFIG_SFTPTEST_PASS="+obscure.MustObscure(testPass),
+	)
+	out, err := cmd.CombinedOutput()
+	if len(out) != 0 {
+		t.Logf("\n----------\n%s----------\n", string(out))
+	}
+	assert.NoError(t, err, "Running sftp integration tests")
+}
