be8c23f0b4da6bec2505baf78df37614464ba694
Dan Walters <dan@walters.io>
2019-05-26 12:03:40 -0500

    dlna: use vfsgen for static assets
    As more assets are added, using vfsgen makes things a bit easier.

---
+++ cmd/serve/dlna/data/assets_generate.go
@@ -0,0 +1,24 @@
+//go:generate go run assets_generate.go
+// The "go:generate" directive compiles static assets by running assets_generate.go
+// +build ignore
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/shurcooL/vfsgen"
+)
+
+func main() {
+	var AssetDir http.FileSystem = http.Dir("./static")
+	err := vfsgen.Generate(AssetDir, vfsgen.Options{
+		PackageName:  "data",
+		BuildTags:    "!dev",
+		VariableName: "Assets",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ cmd/serve/dlna/data/data.go
@@ -0,0 +1,4 @@
+//go:generate go run assets_generate.go
+// The "go:generate" directive compiles static assets by running assets_generate.go
+
+package data
