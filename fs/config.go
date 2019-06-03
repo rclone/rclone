65ffdc89e86c8669345030b122d74caec34b582e
garry415 <garry.415@gmail.com>
2019-06-03 13:12:10 -0700

    fs: add --ignore-case-sync for forced case insensitivity - fixes #2773

--- fs/config.go
+++ fs/config.go
@@ -62,6 +62,7 @@ type ConfigInfo struct {
 	MaxDepth               int
 	IgnoreSize             bool
 	IgnoreChecksum         bool
+	IgnoreCaseSync         bool
 	NoTraverse             bool
 	NoUpdateModTime        bool
 	DataRateUnit           string
