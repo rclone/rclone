rclone/rclone

commit 8a219b3161d1a6bbde9dfe7e4910747a7106a46a

moveto: fix case-insensitive same remote move

fs/operations/operations.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"path"
 	"path/filepath"
 	"sort"
@@ -423,8 +424,8 @@ func Move(fdst fs.Fs, dst fs.Object, remote string, src fs.Object) (newDst fs.Ob
 	}
 	// See if we have Move available
 	if doMove := fdst.Features().Move; doMove != nil && (SameConfig(src.Fs(), fdst) || (SameRemoteType(src.Fs(), fdst) && fdst.Features().ServerSideAcrossConfigs)) {
-		// Delete destination if it exists
-		if dst != nil {
+		// Delete destination if it exists and is not the same file as src (could be same file while seemingly different if the remote is case insensitive)
+		if dst != nil && (!fdst.Features().CaseInsensitive || strings.ToLower(dst.Remote()) != strings.ToLower(src.Remote())) {
 			err = DeleteFile(dst)
 			if err != nil {
 				return newDst, err
@@ -1478,6 +1479,31 @@ func moveOrCopyFile(fdst fs.Fs, fsrc fs.Fs, dstFileName string, srcFileName stri
 		return err
 	}
 
+	// Special case for changing case of a file on a case insensitive remote
+	// This will move the file to a temporary name then
+	// move it back to the intended destination. This is required
+	// to avoid issues with certain remotes and avoid file deletion.
+	if !cp && fdst.Name() == fsrc.Name() && fdst.Features().CaseInsensitive && dstFileName != srcFileName && strings.ToLower(dstFilePath) == strings.ToLower(srcFilePath) {
+		// Create random name to temporarily move file to
+		tmpObjName := dstFileName + "-rclone-move-" + random(8)
+		_, err := fdst.NewObject(tmpObjName)
+		if err != fs.ErrorObjectNotFound {
+			if err == nil {
+				return errors.New("found an already existing file with a randomly generated name. Try the operation again")
+			}
+			return errors.Wrap(err, "error while attempting to move file to a temporary location")
+		}
+		accounting.Stats.Transferring(srcFileName)
+		tmpObj, err := Op(fdst, nil, tmpObjName, srcObj)
+		if err != nil {
+			accounting.Stats.DoneTransferring(srcFileName, false)
+			return errors.Wrap(err, "error while moving file to temporary location")
+		}
+		_, err = Op(fdst, nil, dstFileName, tmpObj)
+		accounting.Stats.DoneTransferring(srcFileName, err == nil)
+		return err
+	}
+
 	if NeedTransfer(dstObj, srcObj) {
 		// If destination already exists, then we must move it into --backup-dir if required
 		if dstObj != nil && fs.Config.BackupDir != "" {
@@ -1504,6 +1530,17 @@ func moveOrCopyFile(fdst fs.Fs, fsrc fs.Fs, dstFileName string, srcFileName stri
 		defer accounting.Stats.DoneChecking(srcFileName)
 	}
 	return err
+}
+
+// random generates a pseudorandom alphanumeric string
+func random(length int) string {
+	randomOutput := make([]byte, length)
+	possibleCharacters := "123567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	rand.Seed(time.Now().Unix())
+	for i := range randomOutput {
+		randomOutput[i] = possibleCharacters[rand.Intn(len(possibleCharacters))]
+	}
+	return string(randomOutput)
 }
 
 // MoveFile moves a single file possibly to a new name

fs/operations/operations_test.go
@@ -739,6 +739,41 @@ func TestMoveFile(t *testing.T) {
 	fstest.CheckItems(t, r.Fremote, file2)
 }
 
+func TestCaseInsensitiveMoveFile(t *testing.T) {
+	r := fstest.NewRun(t)
+	defer r.Finalise()
+	if !r.Fremote.Features().CaseInsensitive {
+		return
+	}
+
+	file1 := r.WriteFile("file1", "file1 contents", t1)
+	fstest.CheckItems(t, r.Flocal, file1)
+
+	file2 := file1
+	file2.Path = "sub/file2"
+
+	err := operations.MoveFile(r.Fremote, r.Flocal, file2.Path, file1.Path)
+	require.NoError(t, err)
+	fstest.CheckItems(t, r.Flocal)
+	fstest.CheckItems(t, r.Fremote, file2)
+
+	r.WriteFile("file1", "file1 contents", t1)
+	fstest.CheckItems(t, r.Flocal, file1)
+
+	err = operations.MoveFile(r.Fremote, r.Flocal, file2.Path, file1.Path)
+	require.NoError(t, err)
+	fstest.CheckItems(t, r.Flocal)
+	fstest.CheckItems(t, r.Fremote, file2)
+
+	file2Capitalized := file2
+	file2Capitalized.Path = "sub/File2"
+
+	err = operations.MoveFile(r.Fremote, r.Fremote, file2Capitalized.Path, file2.Path)
+	require.NoError(t, err)
+	fstest.CheckItems(t, r.Flocal)
+	fstest.CheckItems(t, r.Fremote, file2Capitalized)
+}
+
 func TestMoveFileBackupDir(t *testing.T) {
 	r := fstest.NewRun(t)
 	defer r.Finalise()

commit 586789705a561a19a14ad930e4e4dd62f5896a55

moveto: fix detection of same file name to include the root
Fixes problem introduced in 8a219b3161d1a6bb

fs/operations/operations.go
@@ -402,6 +402,21 @@ func Copy(f fs.Fs, dst fs.Object, remote string, src fs.Object) (newDst fs.Objec
 	return newDst, err
 }
 
+// SameObject returns true if src and dst could be pointing to the
+// same object.
+func SameObject(src, dst fs.Object) bool {
+	if !SameConfig(src.Fs(), dst.Fs()) {
+		return false
+	}
+	srcPath := path.Join(src.Fs().Root(), src.Remote())
+	dstPath := path.Join(dst.Fs().Root(), dst.Remote())
+	if dst.Fs().Features().CaseInsensitive {
+		srcPath = strings.ToLower(srcPath)
+		dstPath = strings.ToLower(dstPath)
+	}
+	return srcPath == dstPath
+}
+
 // Move src object to dst or fdst if nil.  If dst is nil then it uses
 // remote as the name of the new object.
 //
@@ -425,7 +440,7 @@ func Move(fdst fs.Fs, dst fs.Object, remote string, src fs.Object) (newDst fs.Ob
 	// See if we have Move available
 	if doMove := fdst.Features().Move; doMove != nil && (SameConfig(src.Fs(), fdst) || (SameRemoteType(src.Fs(), fdst) && fdst.Features().ServerSideAcrossConfigs)) {
 		// Delete destination if it exists and is not the same file as src (could be same file while seemingly different if the remote is case insensitive)
-		if dst != nil && (!fdst.Features().CaseInsensitive || strings.ToLower(dst.Remote()) != strings.ToLower(src.Remote())) {
+		if dst != nil && !SameObject(src, dst) {
 			err = DeleteFile(dst)
 			if err != nil {
 				return newDst, err