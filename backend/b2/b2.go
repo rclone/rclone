f7e4fec7a998bed4d80f36d8fe27d653fd3f1a7d
yparitcher <y@paritcher.com>
2019-07-25 03:53:51 -0400

    b2: Fix link sharing #3314

--- backend/b2/b2.go
+++ backend/b2/b2.go
@@ -629,6 +629,10 @@ func (f *Fs) list(ctx context.Context, dir string, recurse bool, prefix string, 
 		if err != nil {
 			return err
 		}
+		// Check that there are matching files
+		if len(response.Files) == 0 {
+			return fs.ErrorDirNotFound
+		}
 		for i := range response.Files {
 			file := &response.Files[i]
 			// Finish if file name no longer has prefix
@@ -1226,6 +1230,15 @@ func (f *Fs) PublicLink(ctx context.Context, remote string) (link string, err er
 		RootURL = f.info.DownloadURL
 	} else {
 		RootURL = f.opt.DownloadURL
+	}
+	_, err = f.NewObject(ctx, remote)
+	if err == fs.ErrorObjectNotFound || err == fs.ErrorNotAFile {
+		err = f.list(ctx, remote, false, "", 1, f.opt.Versions, func(remote string, object *api.File, isDirectory bool) error {
+			return errEndList
+		})
+	}
+	if err != nil {
+		return "", err
 	}
 	absPath := "/" + path.Join(f.root, remote)
 	link = RootURL + "/file/" + urlEncode(f.bucket) + absPath
