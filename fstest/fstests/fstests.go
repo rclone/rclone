498a39e96bfa44ef11e92c47353c641ed0ca88b5
Nick Craig-Wood <nick@craig-wood.com>
2019-07-27 16:17:45 +0100

    fstests: allow Purge to fail with ErrorDirNotFound

--- fstest/fstests/fstests.go
+++ fstest/fstests/fstests.go
@@ -1793,7 +1793,9 @@ func Run(t *testing.T, opt *Opt) {
 
 		// Purge the folder
 		err = operations.Purge(ctx, remote, "")
-		require.NoError(t, err)
+		if errors.Cause(err) != fs.ErrorDirNotFound {
+			require.NoError(t, err)
+		}
 		purged = true
 		fstest.CheckListing(t, remote, []fstest.Item{})
 
