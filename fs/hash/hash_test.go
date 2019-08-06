7ad4e319acceebc0f27dabd541e726e0cac5f453
Cenk Alti <cenkalti@gmail.com>
2019-08-06 15:39:29 +0300

    hash: add CRC-32 support

--- fs/hash/hash_test.go
+++ fs/hash/hash_test.go
@@ -74,6 +74,7 @@ var hashTestSet = []hashTest{
 			hash.Dropbox:      "214d2fcf3566e94c99ad2f59bd993daca46d8521a0c447adf4b324f53fddc0c7",
 			hash.QuickXorHash: "0110c000085000031c0001095ec00218d0000700",
 			hash.Whirlpool:    "eddf52133d4566d763f716e853d6e4efbabd29e2c2e63f56747b1596172851d34c2df9944beb6640dbdbe3d9b4eb61180720a79e3d15baff31c91e43d63869a4",
+			hash.CRC32:        "a6041d7e",
 		},
 	},
 	// Empty data set
@@ -85,6 +86,7 @@ var hashTestSet = []hashTest{
 			hash.Dropbox:      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
 			hash.QuickXorHash: "0000000000000000000000000000000000000000",
 			hash.Whirlpool:    "19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3",
+			hash.CRC32:        "00000000",
 		},
 	},
 }
