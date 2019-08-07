67ead727cdf8d7408ad71c74653edc7b720d890f
Nick Craig-Wood <nick@craig-wood.com>
2019-08-07 17:39:09 +0100

    cmd: shorten the locking window when using --progress to avoid deadlock
    Before this change, using -P occasionally deadlocked on the progress
    mutex and the stats mutex since they call each other.
    
    This is fixed by shortening the locking window in the progress routine
    so as not to include the stats calculation.

--- cmd/progress.go
+++ cmd/progress.go
@@ -84,9 +84,6 @@ var (
 
 // printProgress prints the progress with an optional log
 func printProgress(logMessage string) {
-	progressMu.Lock()
-	defer progressMu.Unlock()
-
 	var buf bytes.Buffer
 	w, h, err := terminal.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
@@ -99,6 +96,10 @@ func printProgress(logMessage string) {
 	out := func(s string) {
 		buf.WriteString(s)
 	}
+
+	// Lock from here to prevent terminal corruption
+	progressMu.Lock()
+	progressMu.Unlock()
 
 	if logMessage != "" {
 		out("\n")
