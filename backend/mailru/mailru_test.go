f71256ba91bdb9976b8fbee11799423eefa861bb
Ivan Andreev <ivandeex@gmail.com>
2019-08-29 22:32:46 +0300

    mailru: make name of test remote configurable

--- backend/mailru/mailru_test.go
+++ backend/mailru/mailru_test.go
@@ -5,13 +5,18 @@ import (
 	"testing"
 
 	"github.com/rclone/rclone/backend/mailru"
+	"github.com/rclone/rclone/fstest"
 	"github.com/rclone/rclone/fstest/fstests"
 )
 
 // TestIntegration runs integration tests against the remote
 func TestIntegration(t *testing.T) {
+	remoteName := *fstest.RemoteName // from the -remote cli option
+	if remoteName == "" {
+		remoteName = "TestMailru:"
+	}
 	fstests.Run(t, &fstests.Opt{
-		RemoteName:               "TestMailru:",
+		RemoteName:               remoteName,
 		NilObject:                (*mailru.Object)(nil),
 		SkipBadWindowsCharacters: true,
 	})
