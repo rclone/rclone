865cb4fb45b49aeabcc20eec14c67e422d1f5b8c
Dan Walters <dan@walters.io>
2019-09-15 11:44:28 -0500

    dlna: move root descriptor xml template to the static assets
    Reduce binary size.

--- cmd/serve/dlna/data/data.go
+++ cmd/serve/dlna/data/data.go
@@ -2,3 +2,35 @@
 // The "go:generate" directive compiles static assets by running assets_generate.go
 
 package data
+
+import (
+	"io/ioutil"
+	"text/template"
+
+	"github.com/pkg/errors"
+	"github.com/rclone/rclone/fs"
+)
+
+// GetTemplate returns the rootDesc XML template
+func GetTemplate() (tpl *template.Template, err error) {
+	templateFile, err := Assets.Open("rootDesc.xml.tmpl")
+	if err != nil {
+		return nil, errors.Wrap(err, "get template open")
+	}
+
+	defer fs.CheckClose(templateFile, &err)
+
+	templateBytes, err := ioutil.ReadAll(templateFile)
+	if err != nil {
+		return nil, errors.Wrap(err, "get template read")
+	}
+
+	var templateString = string(templateBytes)
+
+	tpl, err = template.New("rootDesc").Parse(templateString)
+	if err != nil {
+		return nil, errors.Wrap(err, "get template parse")
+	}
+
+	return
+}
