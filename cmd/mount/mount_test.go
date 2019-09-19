1219f19913e45420f54173fc44f4c8c674acc7db
Nick Craig-Wood <nick@craig-wood.com>
2019-09-19 15:10:51 +0100

    mount: skip tests on <= 2 CPUs to avoid lockup in #3154

--- cmd/mount/mount_test.go
+++ cmd/mount/mount_test.go
@@ -3,11 +3,15 @@
 package mount
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/rclone/rclone/cmd/mountlib/mounttest"
 )
 
 func TestMount(t *testing.T) {
+	if runtime.NumCPU() <= 2 {
+		t.Skip("FIXME skipping mount tests as they lock up on <= 2 CPUs - See: https://github.com/rclone/rclone/issues/3154")
+	}
 	mounttest.RunTests(t, mount)
 }
