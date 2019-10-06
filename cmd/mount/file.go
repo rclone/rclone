c00d42600ea74b9e47a2f8deda366248c7e26c9a
Nick Craig-Wood <nick@craig-wood.com>
2019-10-06 12:06:26 +0100

    mount: fix panic on File.Open - Fixes #3595
    This problem was introduced in "mount: allow files of unkown size to
    be read properly" 6d0711887f592b84 by failure to check that the
    DirEntry was nil or not.

--- cmd/mount/file.go
+++ cmd/mount/file.go
@@ -74,7 +74,7 @@ func (f *File) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenR
 	}
 
 	// If size unknown then use direct io to read
-	if handle.Node().DirEntry().Size() < 0 {
+	if entry := handle.Node().DirEntry(); entry != nil && entry.Size() < 0 {
 		resp.Flags |= fuse.OpenDirectIO
 	}
 
