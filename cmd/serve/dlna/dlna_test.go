572d302620e67bdd7bd3db90eebfd1636cd51220
Dan Walters <dan@walters.io>
2019-10-07 10:25:02 -0500

    dlna: simplify search method for associating subtitles with media nodes
    Seems to be some corner cases that are not being handled, so taking a different
    approach that should be a little more robust.
    
    Also, changing resources to be served under a subpath:  We've been serving
    media at /res?path=%2Fdir%2Ffilename.mp4; change that to be just /r/dir/filename.mp4.
    It's cleaner, easier to reason about, and a necessary first step towards just
    serving the resources via httplib anyway.

--- cmd/serve/dlna/dlna_test.go
+++ cmd/serve/dlna/dlna_test.go
@@ -7,7 +7,6 @@ import (
 	"html"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
 	"testing"
@@ -26,7 +25,7 @@ import (
 
 var (
 	dlnaServer *server
-	testURL    string
+	baseURL    string
 )
 
 const (
@@ -38,7 +37,7 @@ func startServer(t *testing.T, f fs.Fs) {
 	opt.ListenAddr = testBindAddress
 	dlnaServer = newServer(f, &opt)
 	assert.NoError(t, dlnaServer.Serve())
-	testURL = "http://" + dlnaServer.HTTPConn.Addr().String() + "/"
+	baseURL = "http://" + dlnaServer.HTTPConn.Addr().String()
 }
 
 func TestInit(t *testing.T) {
@@ -54,7 +53,7 @@ func TestInit(t *testing.T) {
 
 // Make sure that it serves rootDesc.xml (SCPD in uPnP parlance).
 func TestRootSCPD(t *testing.T) {
-	req, err := http.NewRequest("GET", testURL+"rootDesc.xml", nil)
+	req, err := http.NewRequest("GET", baseURL+rootDescPath, nil)
 	require.NoError(t, err)
 	resp, err := http.DefaultClient.Do(req)
 	require.NoError(t, err)
@@ -73,9 +72,7 @@ func TestRootSCPD(t *testing.T) {
 
 // Make sure that it serves content from the remote.
 func TestServeContent(t *testing.T) {
-	itemPath := "/small_jpeg.jpg"
-	pathQuery := url.QueryEscape(itemPath)
-	req, err := http.NewRequest("GET", testURL+"res?path="+pathQuery, nil)
+	req, err := http.NewRequest("GET", baseURL+resPath+"video.mp4", nil)
 	require.NoError(t, err)
 	resp, err := http.DefaultClient.Do(req)
 	require.NoError(t, err)
@@ -85,7 +82,7 @@ func TestServeContent(t *testing.T) {
 	assert.NoError(t, err)
 
 	// Now compare the contents with the golden file.
-	node, err := dlnaServer.vfs.Stat(itemPath)
+	node, err := dlnaServer.vfs.Stat("/video.mp4")
 	assert.NoError(t, err)
 	goldenFile := node.(*vfs.File)
 	goldenReader, err := goldenFile.Open(os.O_RDONLY)
@@ -100,7 +97,7 @@ func TestServeContent(t *testing.T) {
 // Check that ContentDirectory#Browse returns appropriate metadata on the root container.
 func TestContentDirectoryBrowseMetadata(t *testing.T) {
 	// Sample from: https://github.com/rclone/rclone/issues/3253#issuecomment-524317469
-	req, err := http.NewRequest("POST", testURL+"ctl", strings.NewReader(`
+	req, err := http.NewRequest("POST", baseURL+serviceControlURL, strings.NewReader(`
 <?xml version="1.0" encoding="utf-8"?>
 <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
             s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
@@ -126,7 +123,7 @@ func TestContentDirectoryBrowseMetadata(t *testing.T) {
 	require.Contains(t, string(body), html.EscapeString("<container "))
 	require.NotContains(t, string(body), html.EscapeString("<item "))
 	// with a non-zero childCount
-	require.Contains(t, string(body), html.EscapeString(`childCount="1"`))
+	require.Regexp(t, " childCount=&#34;[1-9]", string(body))
 }
 
 // Check that the X_MS_MediaReceiverRegistrar is faked out properly.
@@ -136,7 +133,7 @@ func TestMediaReceiverRegistrarService(t *testing.T) {
 			Action: []byte("RegisterDevice"),
 		},
 	}
-	req, err := http.NewRequest("POST", testURL+"ctl", bytes.NewReader(mustMarshalXML(env)))
+	req, err := http.NewRequest("POST", baseURL+serviceControlURL, bytes.NewReader(mustMarshalXML(env)))
 	require.NoError(t, err)
 	req.Header.Set("SOAPACTION", `"urn:microsoft.com:service:X_MS_MediaReceiverRegistrar:1#RegisterDevice"`)
 	resp, err := http.DefaultClient.Do(req)
@@ -145,4 +142,62 @@ func TestMediaReceiverRegistrarService(t *testing.T) {
 	body, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
 	require.Contains(t, string(body), "<RegistrationRespMsg>")
+}
+
+// Check that ContentDirectory#Browse returns the expected items.
+func TestContentDirectoryBrowseDirectChildren(t *testing.T) {
+	// First the root...
+	req, err := http.NewRequest("POST", baseURL+serviceControlURL, strings.NewReader(`
+<?xml version="1.0" encoding="utf-8"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+            s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+    <s:Body>
+        <u:Browse xmlns:u="urn:schemas-upnp-org:service:ContentDirectory:1">
+            <ObjectID>0</ObjectID>
+            <BrowseFlag>BrowseDirectChildren</BrowseFlag>
+            <Filter>*</Filter>
+            <StartingIndex>0</StartingIndex>
+            <RequestedCount>0</RequestedCount>
+            <SortCriteria></SortCriteria>
+        </u:Browse>
+    </s:Body>
+</s:Envelope>`))
+	require.NoError(t, err)
+	req.Header.Set("SOAPACTION", `"urn:schemas-upnp-org:service:ContentDirectory:1#Browse"`)
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	// expect video.mp4, video.srt, video.en.srt URLs to be in the DIDL
+	require.Contains(t, string(body), "/r/video.mp4")
+	require.Contains(t, string(body), "/r/video.srt")
+	require.Contains(t, string(body), "/r/video.en.srt")
+
+	// Then a subdirectory
+	req, err = http.NewRequest("POST", baseURL+serviceControlURL, strings.NewReader(`
+<?xml version="1.0" encoding="utf-8"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+            s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+    <s:Body>
+        <u:Browse xmlns:u="urn:schemas-upnp-org:service:ContentDirectory:1">
+            <ObjectID>%2Fsubdir</ObjectID>
+            <BrowseFlag>BrowseDirectChildren</BrowseFlag>
+            <Filter>*</Filter>
+            <StartingIndex>0</StartingIndex>
+            <RequestedCount>0</RequestedCount>
+            <SortCriteria></SortCriteria>
+        </u:Browse>
+    </s:Body>
+</s:Envelope>`))
+	require.NoError(t, err)
+	req.Header.Set("SOAPACTION", `"urn:schemas-upnp-org:service:ContentDirectory:1#Browse"`)
+	resp, err = http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	body, err = ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	// expect video.mp4, video.srt, URLs to be in the DIDL
+	require.Contains(t, string(body), "/r/subdir/video.mp4")
+	require.Contains(t, string(body), "/r/subdir/video.srt")
 }
