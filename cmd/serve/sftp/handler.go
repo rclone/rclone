rclone/rclone

commit aeef19b63f340a2e738fc4f3070bfca9f98effe5

serve sftp: fix crash on unsupported operations (eg Readlink)
Before this change the sftp handler returned a nil error for unknown
operations which meant the server crashed when one was encountered.

In particular the "Readlink" operations was causing problems.

After this change the handler returns ErrSshFxOpUnsupported which
signals to the remote end that we don't support that operation.

See: https://forum.rclone.org/t/rclone-serve-sftp-not-working-in-windows/12209

cmd/serve/sftp/handler.go
@@ -94,7 +94,7 @@ func (v vfsHandler) Filecmd(r *sftp.Request) error {
 		// link.symlink = r.Filepath
 		// v.files[r.Target] = link
 	}
-	return nil
+	return sftp.ErrSshFxOpUnsupported
 }
 
 type listerat []os.FileInfo
@@ -150,5 +150,5 @@ func (v vfsHandler) Filelist(r *sftp.Request) (l sftp.ListerAt, err error) {
 		// }
 		// return listerat([]os.FileInfo{file}), nil
 	}
-	return nil, nil
+	return nil, sftp.ErrSshFxOpUnsupported
 }

commit e1f2fd5892a6486598fc316663a374db2e0c9460

sftp: fix test failures
This was introduced by aeef19b63f340a2e

cmd/serve/sftp/handler.go
@@ -93,8 +93,9 @@ func (v vfsHandler) Filecmd(r *sftp.Request) error {
 		// link := newMemFile(r.Target, false)
 		// link.symlink = r.Filepath
 		// v.files[r.Target] = link
+		return sftp.ErrSshFxOpUnsupported
 	}
-	return sftp.ErrSshFxOpUnsupported
+	return nil
 }
 
 type listerat []os.FileInfo