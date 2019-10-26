4499b08afcdae64349994df95542d5133ba4efcf
Nick Craig-Wood <nick@craig-wood.com>
2019-10-26 22:01:07 +0100

    drive: log an ERROR if an incomplete search is returned

--- backend/drive/drive.go
+++ backend/drive/drive.go
@@ -744,7 +744,7 @@ func (f *Fs) list(ctx context.Context, dirIDs []string, title string, directorie
 		fields += ",quotaBytesUsed"
 	}
 
-	fields = fmt.Sprintf("files(%s),nextPageToken", fields)
+	fields = fmt.Sprintf("files(%s),nextPageToken,incompleteSearch", fields)
 
 OUTER:
 	for {
@@ -755,6 +755,9 @@ OUTER:
 		})
 		if err != nil {
 			return false, errors.Wrap(err, "couldn't list directory")
+		}
+		if files.IncompleteSearch {
+			fs.Errorf(f, "search result INCOMPLETE")
 		}
 		for _, item := range files.Files {
 			item.Name = f.opt.Enc.ToStandardName(item.Name)
