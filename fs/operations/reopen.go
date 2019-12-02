4537d9b5cf2982bbeeec828c15e77ee524e2f414
Nick Craig-Wood <nick@craig-wood.com>
2019-12-02 16:04:03 +0000

    operations: make reopen code error on NoLowLevelRetry errors - fixes #3777

--- fs/operations/reopen.go
+++ fs/operations/reopen.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/fs/fserrors"
 )
 
 // reOpen is a wrapper for an object reader which reopens the stream on error
@@ -104,7 +105,7 @@ func (h *reOpen) Read(p []byte) (n int, err error) {
 		h.err = err
 	}
 	h.read += int64(n)
-	if err != nil && err != io.EOF {
+	if err != nil && err != io.EOF && !fserrors.IsNoLowLevelRetryError(err) {
 		// close underlying stream
 		h.opened = false
 		_ = h.rc.Close()
