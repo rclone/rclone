rclone/rclone

commit 10a6a92e524ac37812913c410218a1c7c1db4446

accounting: reset bytes read during copy retry - fixes #4178
During a copy/sync command, if an operation fails due to a network
issue and is retried, the underlying io.Reader is re-initialised,
but the stats for bytes already read are not reset, leading to incorrect
stats. THis was fixed by resetting the bytes read when an Account is
re-initialized.

fs/accounting/accounting.go
@@ -138,6 +138,8 @@ func (acc *Account) UpdateReader(in io.ReadCloser) {
 	acc.close = in
 	acc.origIn = in
 	acc.closed = false
+	acc.lpBytes = 0
+	acc.bytes = int64(0)
 	if withBuf {
 		acc.WithBuffer()
 	}

commit 177195aeeb3c6d04ac54a30036651ac7f73be791

accounting: fix race clearing stats
This race was introduced by

10a6a92e524ac378 accounting: reset bytes read during copy retry

fs/accounting/accounting.go
@@ -146,12 +146,16 @@ func (acc *Account) UpdateReader(in io.ReadCloser) {
 	acc.close = in
 	acc.origIn = in
 	acc.closed = false
-	acc.lpBytes = 0
-	acc.bytes = int64(0)
 	if withBuf {
 		acc.WithBuffer()
 	}
 	acc.mu.Unlock()
+
+	// Reset counter to stop percentage going over 100%
+	acc.values.mu.Lock()
+	acc.values.lpBytes = 0
+	acc.values.bytes = 0
+	acc.values.mu.Unlock()
 }
 
 // averageLoop calculates averages for the stats in the background