863695872a7fbab29007be2409ec761e0bbae161
Nick Craig-Wood <nick@craig-wood.com>
2020-05-17 17:56:02 +0100

    docs: layout tweaks

--- docs/static/js/custom.js
+++ docs/static/js/custom.js
@@ -2,7 +2,7 @@
 
 // Add hover links on headings
 $(function() {
-    return $("h2, h3, h4, h5, h6").each(function(i, el) {
+  return $("h2, h3, h4, h5, h6").each(function(i, el) {
         var $el, icon, id;
         $el = $(el);
         id = $el.attr('id');
