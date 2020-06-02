90f75d62f4427f76bcdb5e50f41cd3dcb796cb51
Nick Craig-Wood <nick@craig-wood.com>
2020-06-02 19:25:59 +0100

    vfs: fix OS vs Unix path confusion - fixes ChangeNotify on Windows
    See: https://forum.rclone.org/t/windows-mount-polling-not-recognising-all-changes-made-by-another-box/16708

--- vfs/vfscommon/path.go
+++ vfs/vfscommon/path.go
@@ -1,10 +1,24 @@
 package vfscommon
 
-import "path/filepath"
+import (
+	"path"
+	"path/filepath"
+)
+
+// OsFindParent returns the parent directory of name, or "" for the
+// root for OS native paths.
+func OsFindParent(name string) string {
+	parent := filepath.Dir(name)
+	if parent == "." || parent == "/" {
+		parent = ""
+	}
+	return parent
+}
 
 // FindParent returns the parent directory of name, or "" for the root
+// for rclone paths.
 func FindParent(name string) string {
-	parent := filepath.Dir(name)
+	parent := path.Dir(name)
 	if parent == "." || parent == "/" {
 		parent = ""
 	}
