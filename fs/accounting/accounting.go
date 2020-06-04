122a47fba655704b89f8c9cb94f85f12f42efda2
Nick Craig-Wood <nick@craig-wood.com>
2020-06-04 15:32:17 +0100

    accounting: Allow transfers to be canceled with context #3257
    This makes all transfers cancelable even if the backend doesn't
    support context as all transfers are done using the Accounting
    framework.

--- fs/accounting/accounting.go
+++ fs/accounting/accounting.go
@@ -216,6 +216,10 @@ func (acc *Account) averageLoop() {
 // Check the read before it has happened is valid returning the number
 // of bytes remaining to read.
 func (acc *Account) checkReadBefore() (bytesUntilLimit int64, err error) {
+	// Check to see if context is cancelled
+	if err = acc.ctx.Err(); err != nil {
+		return 0, err
+	}
 	acc.values.mu.Lock()
 	if acc.values.max >= 0 {
 		bytesUntilLimit = acc.values.max - acc.stats.GetBytes()
@@ -235,7 +239,7 @@ func (acc *Account) checkReadBefore() (bytesUntilLimit int64, err error) {
 }
 
 // Check the read call after the read has happened
-func checkReadAfter(bytesUntilLimit int64, n int, err error) (outN int, outErr error) {
+func (acc *Account) checkReadAfter(bytesUntilLimit int64, n int, err error) (outN int, outErr error) {
 	bytesUntilLimit -= int64(n)
 	if bytesUntilLimit < 0 {
 		// chop the overage off
@@ -304,7 +308,7 @@ func (acc *Account) read(in io.Reader, p []byte) (n int, err error) {
 	if err == nil {
 		n, err = in.Read(p)
 		acc.accountRead(n)
-		n, err = checkReadAfter(bytesUntilLimit, n, err)
+		n, err = acc.checkReadAfter(bytesUntilLimit, n, err)
 	}
 	return n, err
 }
@@ -333,7 +337,7 @@ func (awt *accountWriteTo) Write(p []byte) (n int, err error) {
 	bytesUntilLimit, err := awt.acc.checkReadBefore()
 	if err == nil {
 		n, err = awt.w.Write(p)
-		n, err = checkReadAfter(bytesUntilLimit, n, err)
+		n, err = awt.acc.checkReadAfter(bytesUntilLimit, n, err)
 		awt.acc.accountRead(n)
 	}
 	return n, err
@@ -361,7 +365,7 @@ func (acc *Account) AccountRead(n int) (err error) {
 	defer acc.mu.Unlock()
 	bytesUntilLimit, err := acc.checkReadBefore()
 	if err == nil {
-		n, err = checkReadAfter(bytesUntilLimit, n, err)
+		n, err = acc.checkReadAfter(bytesUntilLimit, n, err)
 		acc.accountRead(n)
 	}
 	return err
