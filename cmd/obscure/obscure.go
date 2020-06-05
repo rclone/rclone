4c7f7582fdab6f1eca35962e7682e7fcf7fee52c
Nick Craig-Wood <nick@craig-wood.com>
2020-06-05 12:48:28 +0100

    obscure: write more help as we are referencing it elsewhere

--- cmd/obscure/obscure.go
+++ cmd/obscure/obscure.go
@@ -14,7 +14,21 @@ func init() {
 
 var commandDefinition = &cobra.Command{
 	Use:   "obscure password",
-	Short: `Obscure password for use in the rclone.conf`,
+	Short: `Obscure password for use in the rclone config file`,
+	Long: `In the rclone config file, human readable passwords are
+obscured. Obscuring them is done by encrypting them and writing them
+out in base64. This is **not** a secure way of encrypting these
+passwords as rclone can decrypt them - it is to prevent "eyedropping"
+- namely someone seeing a password in the rclone config file by
+accident.
+
+Many equally important things (like access tokens) are not obscured in
+the config file. However it is very hard to shoulder surf a 64
+character hex token.
+
+If you want to encrypt the config file then please use config file
+encryption - see [rclone config](/commands/rclone_config/) for more
+info.`,
 	Run: func(command *cobra.Command, args []string) {
 		cmd.CheckArgs(1, 1, command, args)
 		cmd.Run(false, false, command, func() error {
