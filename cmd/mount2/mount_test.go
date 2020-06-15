4808958f93a9ad910f83fda5417caee51e8e0a5c
Nick Craig-Wood <nick@craig-wood.com>
2020-06-15 21:31:11 +0100

    mount2,cmount: skip unreliable tests #4171

--- cmd/mount2/mount_test.go
+++ cmd/mount2/mount_test.go
@@ -5,9 +5,11 @@ package mount2
 import (
 	"testing"
 
+	"github.com/rclone/rclone/fstest/testy"
 	"github.com/rclone/rclone/vfs/vfstest"
 )
 
 func TestMount(t *testing.T) {
+	testy.SkipUnreliable(t)
 	vfstest.RunTests(t, false, mount)
 }
