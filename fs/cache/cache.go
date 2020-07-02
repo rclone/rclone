8d5bc7f28b8459a0e04fdbfd2dca43c5d9ec4555
Nick Craig-Wood <nick@craig-wood.com>
2020-07-02 10:15:47 +0100

    fs/cache: fix moveto/copyto remote:file remote:file2
    Before this change, if the cache was given a source `remote:file` it
    stored `remote:` with the error `fs.ErrorIsFile` attached. This meant
    that if it `remote:` was subsequently looked up it would return the
    `fs.ErrorIsFile` error.
    
    This broke `moveto remote:file remote:file2` as moveto would lookup
    `remote:` from the second argument and erroneously get the
    `fs.ErrorIsFile` error.
    
    This likely broke other commands too.
    
    This was broken in
    
    4c9836035 fs/cache: Add Pin and Unpin and canonicalised lookup
    
    Which was released in v1.52.0
    
    The fix is to make a new cache entry for `remote:` with no error
    attached in the case that the original call returned `fs.ErrorIsFile`.

--- fs/cache/cache.go
+++ fs/cache/cache.go
@@ -56,12 +56,20 @@ func GetFn(fsString string, create func(fsString string) (fs.Fs, error)) (f fs.F
 	if created {
 		canonicalName := fs.ConfigString(f)
 		if canonicalName != fsString {
-			fs.Debugf(nil, "fs cache: renaming cache item %q to be canonical %q", fsString, canonicalName)
-			value, found := c.Rename(fsString, canonicalName)
-			if found {
-				f = value.(fs.Fs)
+			// Note that if err == fs.ErrorIsFile at this moment
+			// then we can't rename the remote as it will have the
+			// wrong error status, we need to add a new one.
+			if err == nil {
+				fs.Debugf(nil, "fs cache: renaming cache item %q to be canonical %q", fsString, canonicalName)
+				value, found := c.Rename(fsString, canonicalName)
+				if found {
+					f = value.(fs.Fs)
+				}
+				addMapping(fsString, canonicalName)
+			} else {
+				fs.Debugf(nil, "fs cache: adding new entry for parent of %q, %q", fsString, canonicalName)
+				Put(canonicalName, f)
 			}
-			addMapping(fsString, canonicalName)
 		}
 	}
 	return f, err
