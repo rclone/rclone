d944bfd9361f16e2d7f7a092392bb0bc5ba6d3db
Nick Craig-Wood <nick@craig-wood.com>
2020-09-04 13:41:48 +0100

    build: explicitly set ARM version to fix build #4553

--- bin/cross-compile.go
+++ bin/cross-compile.go
@@ -74,6 +74,7 @@ var archFlags = map[string][]string{
 	"386":    {"GO386=387"},
 	"mips":   {"GOMIPS=softfloat"},
 	"mipsle": {"GOMIPS=softfloat"},
+	"arm":    {"GOARM=6"},
 	"arm-v7": {"GOARM=7"},
 }
 
@@ -280,7 +281,7 @@ func stripVersion(goarch string) string {
 
 // build the binary in dir returning success or failure
 func compileArch(version, goos, goarch, dir string) bool {
-	log.Printf("Compiling %s/%s", goos, goarch)
+	log.Printf("Compiling %s/%s into %s", goos, goarch, dir)
 	output := filepath.Join(dir, "rclone")
 	if goos == "windows" {
 		output += ".exe"
