fa2777a0bed37cda1fe270b91d91e794c4da40c0
Nick Craig-Wood <nick@craig-wood.com>
2020-11-10 18:01:15 +0000

    testserver: Make Test{FTP,SFTP,Webdav}Rclone run the current rclone
    Before this change the tests were run against the previous stable
    rclone/rclone docker image.
    
    This unfortunately masked errors in the integration test server.
    
    This change uses the currently installed rclone to run "rclone serve
    ftp" etc. This is installed out of the current code by the integration
    test server so will make a better test.

---
+++ fstest/testserver/init.d/rclone-serve.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# start an "rclone serve" server
+
+PIDFILE=/tmp/${NAME}.pid
+DATADIR=/tmp/${NAME}-data
+
+stop() {
+    if status ; then
+        pid=$(cat ${PIDFILE})
+        kill $pid
+        rm ${PIDFILE}
+        echo "$NAME stopped"
+    fi
+}
+
+status() {
+    if [ -e ${PIDFILE} ]; then
+        pid=$(cat ${PIDFILE})
+        if kill -0 &>1 > /dev/null $pid; then
+            # echo "$NAME running"
+            return 0
+        else
+            rm ${PIDFILE}
+        fi
+    fi
+    # echo "$NAME not running"
+    return 1
+}
+
+run() {
+    if ! status ; then
+        mkdir -p ${DATADIR}
+        nohup "$@" >>/tmp/${NAME}.log 2>&1 </dev/null &
+        pid=$!
+        echo $pid > ${PIDFILE}
+        disown $pid
+    fi
+}
+
+. $(dirname "$0")/run.bash
