d484c83c25db2f8c969a7e726a1acf84aab63fec
Manish Gupta <manishgupta.ait@gmail.com>
2020-11-11 11:12:33 +0530

    local: Added code to continue Listing files/folders in case when a circular symlink is observed. Fixes #4743

---
+++ backend/local/symlink.go
@@ -0,0 +1,20 @@
+package local
+
+import (
+	"os"
+	"syscall"
+)
+
+// isCircularSymlinkError checks if the current error code is because of a circular symlink
+func isCircularSymlinkError(err error) bool {
+	if err != nil {
+		if newerr, ok := err.(*os.PathError); ok {
+			if errcode, ok := newerr.Err.(syscall.Errno); ok {
+				if errcode == syscall.ELOOP {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
