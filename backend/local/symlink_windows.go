125168c5bfaa823a0718fb42f2e31e5f2ee8b851
Manish Gupta <manishgupta.ait@gmail.com>
2020-11-11 14:18:31 +0530

    local: Fix Typo.

--- backend/local/symlink_windows.go
+++ backend/local/symlink_windows.go
@@ -9,7 +9,7 @@ import (
 // isCircularSymlinkError checks if the current error code is because of a circular symlink
 func isCircularSymlinkError(err error) bool {
 	if err != nil {
-		if strings.Contains(errcode, "The name of the file cannot be resolved by the system") {
+		if strings.Contains(err.Error(), "The name of the file cannot be resolved by the system") {
 			return true
 		}
 	}
