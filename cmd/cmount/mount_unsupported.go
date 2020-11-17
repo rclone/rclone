86cd5230d7379bb8b61fdaf093d90aa96474f923
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2020-11-17 22:33:12 +0530

    cmount: Add optional `brew` tag to throw an error when using mount in the binaries installed via Homebrew - Fixes #4775
    Signed-off-by: Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>

--- cmd/cmount/mount_unsupported.go
+++ cmd/cmount/mount_unsupported.go
@@ -1,6 +1,6 @@
 // Build for cmount for unsupported platforms to stop go complaining
 // about "no buildable Go source files "
 
-// +build !linux,!darwin,!freebsd,!windows !cgo !cmount
+// +build !linux,!darwin,!freebsd,!windows !brew !cgo !cmount
 
 package cmount
--- cmd/mount/mount_unsupported.go
+++ cmd/mount/mount_unsupported.go
@@ -1,7 +1,7 @@
 // Build for mount for unsupported platforms to stop go complaining
 // about "no buildable Go source files "
 
-// Invert the build constraint: linux,go1.13 darwin,go1.13 freebsd,go1.13
+// Invert the build constraint: linux,go1.13 freebsd,go1.13
 //
 // !((linux&&go1.13) || (darwin&&go1.13) || (freebsd&&go1.13))
 // == !(linux&&go1.13) && !(darwin&&go1.13) && !(freebsd&&go1.13))
