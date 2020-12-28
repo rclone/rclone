0024afc1cde7cbc524c310fcff2fa471458a366b
Benjamin Gustin <gustin.ben@gmail.com>
2020-12-29 00:07:12 +0100

    fs/log: don't compile systemd log integration for non unix systems

---
+++ fs/log/systemd.go
@@ -0,0 +1,16 @@
+// Systemd interface for non-Unix variants only
+
+// +build windows nacl plan9
+
+package log
+
+import (
+	"log"
+	"runtime"
+)
+
+// Enables systemd logs if configured or if auto detected
+func startSystemdLog() bool {
+	log.Fatalf("--log-systemd not supported on %s platform", runtime.GOOS)
+	return false
+}
