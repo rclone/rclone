306b43acb3162076277c12f23721866fdd9e9035
lluuaapp <266615+lluuaapp@users.noreply.github.com>
2021-01-25 18:00:43 +0100

    b2: fixed possible crash when accessing Backblaze b2 remote

--- backend/b2/b2.go
+++ backend/b2/b2.go
@@ -708,7 +708,7 @@ func (f *Fs) list(ctx context.Context, bucket, directory, prefix string, addBuck
 			remote := file.Name[len(prefix):]
 			// Check for directory
 			isDirectory := remote == "" || strings.HasSuffix(remote, "/")
-			if isDirectory {
+			if isDirectory && len(remote) > 1 {
 				remote = remote[:len(remote)-1]
 			}
 			if addBucket {
