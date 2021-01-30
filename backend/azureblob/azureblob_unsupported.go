3b1122c88821da34945a57e570e1f8ebba3a7063
Nick Craig-Wood <nick@craig-wood.com>
2021-01-30 18:01:12 +0000

    azureblob: require go1.14+ to compile due to SDK changes

--- backend/azureblob/azureblob_unsupported.go
+++ backend/azureblob/azureblob_unsupported.go
@@ -1,6 +1,6 @@
 // Build for azureblob for unsupported platforms to stop go complaining
 // about "no buildable Go source files "
 
-// +build plan9 solaris js !go1.13
+// +build plan9 solaris js !go1.14
 
 package azureblob
