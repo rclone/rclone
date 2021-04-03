8c4b06da792180d2c192f050389bc48c6f91ee5c
albertony <12441419+albertony@users.noreply.github.com>
2021-04-03 13:56:15 +0200

    tree: option to print human-readable sizes removed in favor of global option

--- cmd/tree/tree.go
+++ cmd/tree/tree.go
@@ -44,7 +44,6 @@ func init() {
 	flags.StringVarP(cmdFlags, &outFileName, "output", "o", "", "Output to file instead of stdout.")
 	// Files
 	flags.BoolVarP(cmdFlags, &opts.ByteSize, "size", "s", false, "Print the size in bytes of each file.")
-	flags.BoolVarP(cmdFlags, &opts.UnitSize, "human", "", false, "Print the size in a more human readable way.")
 	flags.BoolVarP(cmdFlags, &opts.FileMode, "protections", "p", false, "Print the protections for each file.")
 	// flags.BoolVarP(cmdFlags, &opts.ShowUid, "uid", "", false, "Displays file owner or UID number.")
 	// flags.BoolVarP(cmdFlags, &opts.ShowGid, "gid", "", false, "Displays file group owner or GID number.")
@@ -109,6 +108,7 @@ short options as they conflict with rclone's short options.
 		opts.NameSort = sort == "name"
 		opts.SizeSort = sort == "size"
 		ci := fs.GetConfig(context.Background())
+		opts.UnitSize = ci.HumanReadable
 		if opts.DeepLevel == 0 {
 			opts.DeepLevel = ci.MaxDepth
 		}
