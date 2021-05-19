e0a5057f979933b6a0039f64dd438d1cb9729ce6
Florian Penzkofer <fp@nullptr.de>
2021-05-18 20:42:25 -0500

    Escape password string

--- backend/fichier/api.go
+++ backend/fichier/api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
@@ -126,7 +127,7 @@ func (f *Fs) listSharedFiles(ctx context.Context, id string) (entries fs.DirEntr
 	}
 	if f.opt.SharedPassword != "" {
 		opts.Method = "POST"
-		opts.Body = strings.NewReader("json=1&pass=" + f.opt.SharedPassword);
+		opts.Body = strings.NewReader("json=1&pass=" + url.QueryEscape(f.opt.SharedPassword));
 		opts.ContentType = "application/x-www-form-urlencoded"
 		opts.Parameters = nil
 	}
