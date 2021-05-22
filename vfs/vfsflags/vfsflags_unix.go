22e86ce3359a34f299aedd9d11a869ec0fecda74
albertony <12441419+albertony@users.noreply.github.com>
2021-05-22 20:48:02 +0200

    vfs: fix that umask option cannot be set as environment variable (#5351)
    Fixes #5350

--- vfs/vfsflags/vfsflags_unix.go
+++ vfs/vfsflags/vfsflags_unix.go
@@ -10,9 +10,9 @@ import (
 
 // add any extra platform specific flags
 func platformFlags(flagSet *pflag.FlagSet) {
-	flags.IntVarP(flagSet, &Opt.Umask, "umask", "", Opt.Umask, "Override the permission bits set by the filesystem. Not supported on Windows.")
 	Opt.Umask = unix.Umask(0) // read the umask
 	unix.Umask(Opt.Umask)     // set it back to what it was
+	flags.IntVarP(flagSet, &Opt.Umask, "umask", "", Opt.Umask, "Override the permission bits set by the filesystem. Not supported on Windows.")
 	Opt.UID = uint32(unix.Geteuid())
 	Opt.GID = uint32(unix.Getegid())
 	flags.Uint32VarP(flagSet, &Opt.UID, "uid", "", Opt.UID, "Override the uid field set by the filesystem. Not supported on Windows.")
