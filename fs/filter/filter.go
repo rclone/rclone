fe690028f060849a9f3e7b11d890540dcbd42843
Ivan Andreev <ivandeex@gmail.com>
2021-05-22 20:09:53 +0300

    fs/filter: make IncludeRemote public

--- fs/filter/filter.go
+++ fs/filter/filter.go
@@ -373,8 +373,8 @@ func (f *Filter) InActive() bool {
 		len(f.Opt.ExcludeFile) == 0)
 }
 
-// includeRemote returns whether this remote passes the filter rules.
-func (f *Filter) includeRemote(remote string) bool {
+// IncludeRemote returns whether this remote passes the filter rules.
+func (f *Filter) IncludeRemote(remote string) bool {
 	for _, rule := range f.fileRules.rules {
 		if rule.Match(remote) {
 			return rule.Include
@@ -467,7 +467,7 @@ func (f *Filter) Include(remote string, size int64, modTime time.Time) bool {
 	if f.Opt.MaxSize >= 0 && size > int64(f.Opt.MaxSize) {
 		return false
 	}
-	return f.includeRemote(remote)
+	return f.IncludeRemote(remote)
 }
 
 // IncludeObject returns whether this object should be included into
