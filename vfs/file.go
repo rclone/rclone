89ac03608b800bdd6fd5f29dc7e96ede7b59b7f2
Nick Craig-Wood <nick@craig-wood.com>
2021-08-10 18:19:20 +0100

    vfs: fix crash when truncating a just uploaded object - Fixes #5522

--- vfs/file.go
+++ vfs/file.go
@@ -744,7 +744,6 @@ func (f *File) Truncate(size int64) (err error) {
 	f.mu.Lock()
 	writers := make([]Handle, len(f.writers))
 	copy(writers, f.writers)
-	o := f.o
 	f.mu.Unlock()
 
 	// FIXME: handle closing writer
@@ -758,6 +757,12 @@ func (f *File) Truncate(size int64) (err error) {
 				err = truncateErr
 			}
 		}
+		return err
+	}
+
+	// if o is nil it isn't valid yet
+	o, err := f.waitForValidObject()
+	if err != nil {
 		return err
 	}
 
