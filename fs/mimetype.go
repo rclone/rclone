208afbd4a3174b29c3307974f10343389e7f03da
albertony <12441419+albertony@users.noreply.github.com>
2021-09-24 19:47:04 +0200

    Do not override mime types from os defaults
    https://forum.rclone.org/t/rclone-serve-http-save-as/26672

--- fs/mimetype.go
+++ fs/mimetype.go
@@ -7,6 +7,42 @@ import (
 	"strings"
 )
 
+// Add a minimal number of mime types to augment go's built in types
+// for environments which don't have access to a mime.types file (e.g.
+// Termux on android)
+func init() {
+	for _, t := range []struct {
+		mimeType   string
+		extensions string
+	}{
+		{"audio/flac", ".flac"},
+		{"audio/mpeg", ".mpga,.mpega,.mp2,.mp3,.m4a"},
+		{"audio/ogg", ".oga,.ogg,.opus,.spx"},
+		{"audio/x-wav", ".wav"},
+		{"image/tiff", ".tiff,.tif"},
+		{"video/dv", ".dif,.dv"},
+		{"video/fli", ".fli"},
+		{"video/mpeg", ".mpeg,.mpg,.mpe"},
+		{"video/MP2T", ".ts"},
+		{"video/mp4", ".mp4"},
+		{"video/quicktime", ".qt,.mov"},
+		{"video/ogg", ".ogv"},
+		{"video/webm", ".webm"},
+		{"video/x-msvideo", ".avi"},
+		{"video/x-matroska", ".mpv,.mkv"},
+		{"text/srt", ".srt"},
+	} {
+		for _, ext := range strings.Split(t.extensions, ",") {
+			if mime.TypeByExtension(ext) == "" {
+				err := mime.AddExtensionType(ext, t.mimeType)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
 // MimeTypeFromName returns a guess at the mime type from the name
 func MimeTypeFromName(remote string) (mimeType string) {
 	mimeType = mime.TypeByExtension(path.Ext(remote))
