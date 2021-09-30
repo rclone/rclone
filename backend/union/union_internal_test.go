3bf655128fed559a0ced51469c4e8a23f83d93d6
Nick Craig-Wood <nick@craig-wood.com>
2021-09-30 11:11:46 +0100

    union: fix rename not working with union of local disk and bucket based remote
    Before this change the union's feature flags were a strict AND of the
    underlying remotes. This means that a union of a local disk (which can
    Move but not Copy) and a bucket based remote (which can Copy but not
    Move) could neither Move nor Copy.
    
    This fix advertises Move in the union if all the remotes can Move or
    Copy. It also implements Move as Copy+Delete (like rclone does
    normally) if the underlying union does not support Move.
    
    This enables renames to work with unions of local disk and bucket
    based remotes expected.
    
    Fixes #5632

--- backend/union/union_internal_test.go
+++ backend/union/union_internal_test.go
@@ -3,16 +3,37 @@ package union
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/rclone/rclone/fs"
 	"github.com/rclone/rclone/fs/object"
+	"github.com/rclone/rclone/fs/operations"
 	"github.com/rclone/rclone/fstest"
 	"github.com/rclone/rclone/fstest/fstests"
 	"github.com/rclone/rclone/lib/random"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+// MakeTestDirs makes directories in /tmp for testing
+func MakeTestDirs(t *testing.T, n int) (dirs []string, clean func()) {
+	for i := 1; i <= n; i++ {
+		dir, err := ioutil.TempDir("", fmt.Sprintf("rclone-union-test-%d", n))
+		require.NoError(t, err)
+		dirs = append(dirs, dir)
+	}
+	clean = func() {
+		for _, dir := range dirs {
+			err := os.RemoveAll(dir)
+			assert.NoError(t, err)
+		}
+	}
+	return dirs, clean
+}
 
 func (f *Fs) TestInternalReadOnly(t *testing.T) {
 	if f.name != "TestUnionRO" {
@@ -65,3 +86,83 @@ func (f *Fs) InternalTest(t *testing.T) {
 }
 
 var _ fstests.InternalTester = (*Fs)(nil)
+
+// This specifically tests a union of local which can Move but not
+// Copy and :memory: which can Copy but not Move to makes sure that
+// the resulting union can Move
+func TestMoveCopy(t *testing.T) {
+	if *fstest.RemoteName != "" {
+		t.Skip("Skipping as -remote set")
+	}
+	ctx := context.Background()
+	dirs, clean := MakeTestDirs(t, 1)
+	defer clean()
+	fsString := fmt.Sprintf(":union,upstreams='%s :memory:bucket':", dirs[0])
+	f, err := fs.NewFs(ctx, fsString)
+	require.NoError(t, err)
+
+	unionFs := f.(*Fs)
+	fLocal := unionFs.upstreams[0].Fs
+	fMemory := unionFs.upstreams[1].Fs
+
+	t.Run("Features", func(t *testing.T) {
+		assert.NotNil(t, f.Features().Move)
+		assert.Nil(t, f.Features().Copy)
+
+		// Check underlying are as we are expect
+		assert.NotNil(t, fLocal.Features().Move)
+		assert.Nil(t, fLocal.Features().Copy)
+		assert.Nil(t, fMemory.Features().Move)
+		assert.NotNil(t, fMemory.Features().Copy)
+	})
+
+	// Put a file onto the local fs
+	contentsLocal := random.String(50)
+	fileLocal := fstest.NewItem("local.txt", contentsLocal, time.Now())
+	_, _ = fstests.PutTestContents(ctx, t, fLocal, &fileLocal, contentsLocal, true)
+	objLocal, err := f.NewObject(ctx, fileLocal.Path)
+	require.NoError(t, err)
+
+	// Put a file onto the memory fs
+	contentsMemory := random.String(60)
+	fileMemory := fstest.NewItem("memory.txt", contentsMemory, time.Now())
+	_, _ = fstests.PutTestContents(ctx, t, fMemory, &fileMemory, contentsMemory, true)
+	objMemory, err := f.NewObject(ctx, fileMemory.Path)
+	require.NoError(t, err)
+
+	fstest.CheckListing(t, f, []fstest.Item{fileLocal, fileMemory})
+
+	t.Run("MoveLocal", func(t *testing.T) {
+		fileLocal.Path = "local-renamed.txt"
+		_, err := operations.Move(ctx, f, nil, fileLocal.Path, objLocal)
+		require.NoError(t, err)
+		fstest.CheckListing(t, f, []fstest.Item{fileLocal, fileMemory})
+
+		// Check can retrieve object from union
+		obj, err := f.NewObject(ctx, fileLocal.Path)
+		require.NoError(t, err)
+		assert.Equal(t, fileLocal.Size, obj.Size())
+
+		// Check can retrieve object from underlying
+		obj, err = fLocal.NewObject(ctx, fileLocal.Path)
+		require.NoError(t, err)
+		assert.Equal(t, fileLocal.Size, obj.Size())
+
+		t.Run("MoveMemory", func(t *testing.T) {
+			fileMemory.Path = "memory-renamed.txt"
+			_, err := operations.Move(ctx, f, nil, fileMemory.Path, objMemory)
+			require.NoError(t, err)
+			fstest.CheckListing(t, f, []fstest.Item{fileLocal, fileMemory})
+
+			// Check can retrieve object from union
+			obj, err := f.NewObject(ctx, fileMemory.Path)
+			require.NoError(t, err)
+			assert.Equal(t, fileMemory.Size, obj.Size())
+
+			// Check can retrieve object from underlying
+			obj, err = fMemory.NewObject(ctx, fileMemory.Path)
+			require.NoError(t, err)
+			assert.Equal(t, fileMemory.Size, obj.Size())
+		})
+	})
+}
