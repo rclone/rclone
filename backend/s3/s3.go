990c855bc3f3e4aab37dad59c7de3579ef3056c4
Nick Craig-Wood <nick@craig-wood.com>
2021-10-27 15:50:07 +0100

    s3: fix IAM Role for Service Account not working and other auth problems
    This patch reverts this commit
    
    1605f9e14dbbe58e s3: Fix shared_credentials_file auth
    
    It unfortunately had the side effect of making the s3 SDK ignore the
    config in our custom chain and use the default provider. This means
    that advanced auth was being ignored such as --s3-profile with
    role_arn.
    
    Fixes #5468
    Fixes #5762

--- backend/s3/s3.go
+++ backend/s3/s3.go
@@ -1603,6 +1603,9 @@ func s3Connection(ctx context.Context, opt *Options, client *http.Client) (*s3.S
 		awsSessionOpts.SharedConfigState = session.SharedConfigEnable
 		// Set the name of the profile if supplied
 		awsSessionOpts.Profile = opt.Profile
+		// The session constructor (aws/session/mergeConfigSrcs) will only use the user's preferred credential source
+		// (from the shared config file) if the passed-in Options.Config.Credentials is nil.
+		awsSessionOpts.Config.Credentials = nil
 	}
 	ses, err := session.NewSessionWithOptions(awsSessionOpts)
 	if err != nil {
