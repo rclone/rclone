63d36faf59b1ce797b3013ca0470746070db5ea7
albertony <12441419+albertony@users.noreply.github.com>
2022-04-10 13:21:12 +0200

    docs/size: extend documentation of size command

--- cmd/size/size.go
+++ cmd/size/size.go
@@ -24,6 +24,25 @@ func init() {
 var commandDefinition = &cobra.Command{
 	Use:   "size remote:path",
 	Short: `Prints the total size and number of objects in remote:path.`,
+	Long: `
+Counts objects in the path and calculates the total size. Prints the
+result to standard output.
+
+By default the output is in human-readable format, but shows values in
+both human-readable format as well as the raw numbers (global option
+` + "`--human-readable`" + ` is not considered). Use option ` + "`--json`" + `
+to format output as JSON instead.
+
+Recurses by default, use ` + "`--max-depth 1`" + ` to stop the
+recursion.
+
+Some backends do not always provide file sizes, see for example
+[Google Photos](/googlephotos/#size) and
+[Google Drive](/drive/#limitations-of-google-docs).
+Rclone will then show a notice in the log indicating how many such
+files were encountered, and count them in as empty files in the output
+of the size command.
+`,
 	Run: func(command *cobra.Command, args []string) {
 		cmd.CheckArgs(1, 1, command, args)
 		fsrc := cmd.NewFsSrc(args)
