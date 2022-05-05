12abb964e41d742536168a9403f5b9f5e5a37f13
itsToggle <71379623+itsToggle@users.noreply.github.com>
2022-05-05 12:22:33 +0200

    Update types.go

--- backend/premiumizeme/api/types.go
+++ backend/premiumizeme/api/types.go
@@ -31,17 +31,17 @@ const (
 
 // Item refers to a file or folder
 type Item struct {
-	Breadcrumbs     []Breadcrumb `json:"breadcrumbs"`
-	CreatedAt       int64        `json:"created_at,omitempty"`
-	ID              string       `json:"id"`
-	Link            string       `json:"link,omitempty"`
-	Name            string       `json:"name"`
-	Size            int64        `json:"size,omitempty"`
-	StreamLink      string       `json:"stream_link,omitempty"`
-	Type            string       `json:"type"`
-	TranscodeStatus string       `json:"transcode_status"`
-	IP              string       `json:"ip"`
-	MimeType        string       `json:"mime_type"`
+	Breadcrumbs     []Breadcrumb ``                          //`json:"breadcrumbs"`
+	CreatedAt       int64        ``                          //`json:"created_at,omitempty"`
+	ID              string       `json:"id"`                 //`json:"id"`
+	Link            string       `json:"download,omitempty"` //`json:"link,omitempty"`
+	Name            string       `json:"filename"`           //`json:"name"`
+	Size            int64        `json:"filesize,omitempty"` //`json:"size,omitempty"`
+	StreamLink      string       ``                          //`json:"stream_link,omitempty"`
+	Type            string       `json:"type"`               //`json:"type"`
+	TranscodeStatus string       ``                          //`json:"transcode_status"`
+	IP              string       ``                          //`json:"ip"`
+	MimeType        string       `json:"mimeType"`           //`json:"mime_type"`
 }
 
 // Breadcrumb is part the breadcrumb trail for a file or folder.  It
@@ -55,10 +55,10 @@ type Breadcrumb struct {
 // FolderListResponse is the response to folder/list
 type FolderListResponse struct {
 	Response
-	Content  []Item `json:"content"`
-	Name     string `json:"name,omitempty"`
-	ParentID string `json:"parent_id,omitempty"`
-	FolderID string `json:"folder_id,omitempty"`
+	Content  []Item ``                          //`json:"content"`
+	Name     string `json:"filename,omitempty"` //`json:"name,omitempty"`
+	ParentID string ``                          //`json:"parent_id,omitempty"`
+	FolderID string ``                          //`json:"folder_id,omitempty"`
 }
 
 // FolderCreateResponse is the response to folder/create
