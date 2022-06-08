6c2331ffd73096c3957844bd054fc94cdf2654e8
Nick Craig-Wood <nick@craig-wood.com>
2022-06-08 18:01:06 +0100

    mount: skip tests on CI even if >= 2 processors

--- cmd/mount/mount_test.go
+++ cmd/mount/mount_test.go
@@ -7,6 +7,7 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/rclone/rclone/fstest/testy"
 	"github.com/rclone/rclone/vfs/vfstest"
 )
 
@@ -14,5 +15,6 @@ func TestMount(t *testing.T) {
 	if runtime.NumCPU() <= 2 {
 		t.Skip("FIXME skipping mount tests as they lock up on <= 2 CPUs - See: https://github.com/rclone/rclone/issues/3154")
 	}
+	testy.SkipUnreliable(t)
 	vfstest.RunTests(t, false, mount)
 }
