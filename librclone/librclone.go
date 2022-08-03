9d3958bd0b0f26ee597cb732761d705913bacdb1
Nick Craig-Wood <nick@craig-wood.com>
2022-08-03 10:11:16 +0100

    build: fix formatting after golangci-lint update

--- librclone/librclone.go
+++ librclone/librclone.go
@@ -35,12 +35,12 @@ import (
 	"github.com/rclone/rclone/librclone/librclone"
 
 	_ "github.com/rclone/rclone/backend/all"   // import all backends
+	_ "github.com/rclone/rclone/cmd/cmount"    // import cmount
+	_ "github.com/rclone/rclone/cmd/mount"     // import mount
+	_ "github.com/rclone/rclone/cmd/mount2"    // import mount2
 	_ "github.com/rclone/rclone/fs/operations" // import operations/* rc commands
 	_ "github.com/rclone/rclone/fs/sync"       // import sync/*
 	_ "github.com/rclone/rclone/lib/plugin"    // import plugins
-	_ "github.com/rclone/rclone/cmd/mount"     // import mount
-	_ "github.com/rclone/rclone/cmd/mount2"    // import mount2
-	_ "github.com/rclone/rclone/cmd/cmount"    // import cmount
 )
 
 // RcloneInitialize initializes rclone as a library
