ceef78ce4493f8d07f0ad2622a7d52ca98fb24f3
Lorenz Brun <lorenz@brun.one>
2022-09-20 00:31:56 +0200

    vfs: fix directory cache serving stale data
    The VFS directory cache layer didn't update directory entry properties
    if they are reused after cache invalidation.
    
    Update them unconditionally as newDir sets them to the same value and
    setting a pointer is cheaper in both LoC as well as CPU cycles than a
    branch.
    
    Also add a test exercising this behavior.
    
    Fixes #6335

--- vfs/dir.go
+++ vfs/dir.go
@@ -760,6 +760,7 @@ func (d *Dir) _readDirFromEntries(entries fs.DirEntries, dirTree dirtree.DirTree
 			dir := node.(*Dir)
 			dir.mu.Lock()
 			dir.modTime = item.ModTime(context.TODO())
+			dir.entry = item
 			if dirTree != nil {
 				err = dir._readDirFromDirTree(dirTree, when)
 				if err != nil {
