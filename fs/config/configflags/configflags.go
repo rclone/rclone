fbff8be4a85a638a3a477d2bedb489d5a7bccf83
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2022-10-07 00:09:44 +0200

    Added config command flags
    ConfigCommandIn; config-command-in
    ConfigCommandOut; config-command-out

--- fs/config/configflags/configflags.go
+++ fs/config/configflags/configflags.go
@@ -67,6 +67,8 @@ func AddFlags(ci *fs.ConfigInfo, flagSet *pflag.FlagSet) {
 	flags.BoolVarP(flagSet, &ci.InsecureSkipVerify, "no-check-certificate", "", ci.InsecureSkipVerify, "Do not verify the server SSL certificate (insecure)")
 	flags.BoolVarP(flagSet, &ci.AskPassword, "ask-password", "", ci.AskPassword, "Allow prompt for password for encrypted configuration")
 	flags.FVarP(flagSet, &ci.PasswordCommand, "password-command", "", "Command for supplying password for encrypted configuration")
+	flags.FVarP(flagSet, &ci.ConfigCommandIn, "config-command-in", "", "Command for supplying input config file for reading")
+	flags.FVarP(flagSet, &ci.ConfigCommandOut, "config-command-out", "", "Command for supplying output config file for writing")
 	flags.BoolVarP(flagSet, &deleteBefore, "delete-before", "", false, "When synchronizing, delete files on destination before transferring")
 	flags.BoolVarP(flagSet, &deleteDuring, "delete-during", "", false, "When synchronizing, delete files during transfer")
 	flags.BoolVarP(flagSet, &deleteAfter, "delete-after", "", false, "When synchronizing, delete files on destination after transferring (default)")
