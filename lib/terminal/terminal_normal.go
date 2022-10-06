14534c573ae0c501ff57ad81f2645395c43e2cb5
Carl Edquist <edquist@cs.wisc.edu>
2022-10-06 01:27:44 -0500

    terminal: WriteTerminalTitle to terminal not stdout
    The terminal title output should go to the same place as the rest of the
    terminal output.  It's important _not_ to send it to stdout, where it
    would get included with the main program output when redirected to a
    file - for instance with the rclone cat command.

--- lib/terminal/terminal_normal.go
+++ lib/terminal/terminal_normal.go
@@ -34,5 +34,5 @@ func ReadPassword(fd int) ([]byte, error) {
 
 // WriteTerminalTitle writes a string to the terminal title
 func WriteTerminalTitle(title string) {
-	fmt.Printf(ChangeTitle + title + BEL)
+	fmt.Fprintf(TerminalOutput, ChangeTitle + title + BEL)
 }
