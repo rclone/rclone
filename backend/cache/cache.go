c37b6b1a43106b30b3e164775d1e8cf8db570575
Nick Craig-Wood <nick@craig-wood.com>
2023-02-06 10:44:40 +0000

    cache: fix lint error in latest golangci-lint

--- backend/cache/cache.go
+++ backend/cache/cache.go
@@ -1038,7 +1038,7 @@ func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err e
 		}
 		fs.Debugf(dir, "list: remove entry: %v", entryRemote)
 	}
-	entries = nil
+	entries = nil //nolint:ineffassign
 
 	// and then iterate over the ones from source (temp Objects will override source ones)
 	var batchDirectories []*Directory
