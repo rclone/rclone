a7e6806f269bb32607d89ffe5304f1c964ec48c0
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2023-04-11 17:44:37 +0530

    mount2: updated go-fuse version
    Signed-off-by: Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>

--- cmd/mount2/mount_test.go
+++ cmd/mount2/mount_test.go
@@ -1,16 +1,14 @@
-//go:build linux || (darwin && amd64)
-// +build linux darwin,amd64
+//go:build linux
+// +build linux
 
 package mount2
 
 import (
 	"testing"
 
-	"github.com/rclone/rclone/fstest/testy"
 	"github.com/rclone/rclone/vfs/vfstest"
 )
 
 func TestMount(t *testing.T) {
-	testy.SkipUnreliable(t)
 	vfstest.RunTests(t, false, mount)
 }
