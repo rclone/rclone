ee84065f4f6d9ef0b830ec8316730f2add5ec012
Saw-jan <saw.jan.grg3e@gmail.com>
2023-09-12 16:35:13 +0545

    add context to log and fallthrough to error log level

--- cmd/serve/s3/logger.go
+++ cmd/serve/s3/logger.go
@@ -12,15 +12,14 @@ type logger struct{}
 
 // print log message
 func (l logger) Print(level gofakes3.LogLevel, v ...interface{}) {
-	// fs.Infof(nil, fmt.Sprintln(v...))
 	switch level {
-	case gofakes3.LogErr:
-		fs.Errorf(nil, fmt.Sprintln(v...))
-	case gofakes3.LogWarn:
-		fs.Infof(nil, fmt.Sprintln(v...))
-	case gofakes3.LogInfo:
-		fs.Debugf(nil, fmt.Sprintln(v...))
 	default:
-		panic("unknown level")
+		fallthrough
+	case gofakes3.LogErr:
+		fs.Errorf("serve s3", fmt.Sprintln(v...))
+	case gofakes3.LogWarn:
+		fs.Infof("serve s3", fmt.Sprintln(v...))
+	case gofakes3.LogInfo:
+		fs.Debugf("serve s3", fmt.Sprintln(v...))
 	}
 }
