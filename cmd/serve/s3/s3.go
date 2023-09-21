919a27068fab48a8cbc1d2f83e04ddaa5d21855c
Saw-jan <saw.jan.grg3e@gmail.com>
2023-09-21 18:37:14 +0545

    fix go lint

--- cmd/serve/s3/s3.go
+++ cmd/serve/s3/s3.go
@@ -61,7 +61,10 @@ var Command = &cobra.Command{
 			}
 			router := s.Router()
 			s.Bind(router)
-			s.serve()
+			err = s.serve()
+			if err != nil {
+				return err
+			}
 			s.Wait()
 			return nil
 		})
