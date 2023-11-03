ee98e516e5dd95cd4c243dcadb1f6f4cc562d282
albertony <12441419+albertony@users.noreply.github.com>
2023-11-03 20:40:27 +0100

    docs: show hashsum arguments as optional in usage string

--- cmd/hashsum/hashsum.go
+++ cmd/hashsum/hashsum.go
@@ -82,7 +82,7 @@ func CreateFromStdinArg(ht hash.Type, args []string, startArg int) (bool, error)
 }
 
 var commandDefinition = &cobra.Command{
-	Use:   "hashsum <hash> remote:path",
+	Use:   "hashsum [<hash> remote:path]",
 	Short: `Produces a hashsum file for all the objects in the path.`,
 	Long: `
 Produces a hash file for all the objects in the path using the hash
