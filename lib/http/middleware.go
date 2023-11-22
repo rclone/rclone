5fba5025168aac0c9aa5dd767e51e0ee264ebbe5
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-22 13:23:34 +0100

    http: enable methods used with WebDAV - fixes #7444
    Without this, requests like PROPFIND, issued from a browser, fail.

--- lib/http/middleware.go
+++ lib/http/middleware.go
@@ -183,8 +183,8 @@ func MiddlewareCORS(allowOrigin string) Middleware {
 
 			if allowOrigin != "" {
 				w.Header().Add("Access-Control-Allow-Origin", allowOrigin)
-				w.Header().Add("Access-Control-Request-Method", "POST, OPTIONS, GET, HEAD")
 				w.Header().Add("Access-Control-Allow-Headers", "authorization, Content-Type")
+				w.Header().Add("Access-Control-Allow-Methods", "COPY, DELETE, GET, HEAD, LOCK, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, TRACE, UNLOCK")
 			}
 
 			next.ServeHTTP(w, r)
