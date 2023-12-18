ce83687829f429403e51b7f9dee06ed1492891f7
nielash <nielronash@gmail.com>
2023-12-18 11:09:02 -0500

    operations: --no-block-rmdir for disposable files blocking removal of otherwise empty directory
    Before this change, rclone commands that remove empty dirs (including rmdir,
    rmdirs, sync, and bisync) would silently fail to remove dirs if those dirs
    contained files that were excluded by filters. This can pose a problem for some
    automatically created system files, such as `.DS_Store` on macOS, which users
    often prefer to exclude from syncs -- including them makes for noisy logs, but
    excluding them can make it impossible to remove otherwise empty directories.
    
    After this change, a new `--no-block-rmdir` flag allows specifying a
    comma-separated list of such files which rclone should consider "disposable" if
    they block removal of otherwise empty directories. When set, if rclone fails to
    remove a directory, it will check the contents of the directory (ignoring any
    filters.) If the directory contains ONLY files on the disposable list, rclone
    will delete the files and then remove the directory.
    
    See also: partially-related #6437

--- fs/config.go
+++ fs/config.go
@@ -635,6 +635,7 @@ type ConfigInfo struct {
 	Inplace                    bool              `config:"inplace"`      // Download directly to destination file instead of atomic download to temp/rename
 	PartialSuffix              string            `config:"partial_suffix"`
 	MetadataMapper             SpaceSepList      `config:"metadata_mapper"`
+	NoBlockRmdir               CommaSepList      `config:"no_block_rmdir"`
 }
 
 func init() {
