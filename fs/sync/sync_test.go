da7a3902848451f8cdef9dc949b428f4823ef275
nielash <nielronash@gmail.com>
2023-12-28 12:30:47 -0500

    local: add server-side copy with xattrs on macOS (part-fix #1710)
    Before this change, macOS-specific metadata was not preserved by rclone, even for
    local-to-local transfers (it does not use the "user." prefix, nor is Mac metadata
    limited to xattrs.) Additionally, rclone did not take advantage of APFS's native
    "cloning" functionality for fast and deduplicated transfers.
    
    After this change, local (on macOS only) supports "server-side copy" similarly to
    other remotes, and achieves this by using (when possible) macOS's native APFS
    "cloning", which is the same underlying mechanism deployed when a user
    duplicates a file via the Finder UI. This has several advantages over the
    previous behavior:
    
    - It is extremely fast (even large files can be cloned instantly)
    - It is very efficient in terms of storage, as it automatically deduplicates when
    possible (i.e. so that having two identical files does not consume more storage
    than having just one.) (The concept is similar to a "hard link", but subsequent
    modifications will not affect the original file.)
    - It preserves Mac-specific metadata to the maximum degree, including not only
    xattrs but also metadata not easily settable by other methods, including Finder
    and Spotlight params.
    
    When server-side "clone" is not available (for example, on non-APFS volumes), it
    falls back to server-side "copy" (still preserving metadata but using more disk
    storage.) It is only used when both remotes are local (and not wrapped by other
    remotes, such as crypt.) The behavior of local on non-mac systems is unchanged.

--- fs/sync/sync_test.go
+++ fs/sync/sync_test.go
@@ -993,6 +993,12 @@ func testSyncWithMaxDuration(t *testing.T, cutoffMode fs.CutoffMode) {
 	r.CheckLocalItems(t, file1, file2)
 	r.CheckRemoteItems(t)
 
+	if runtime.GOOS == "darwin" {
+		r.Flocal.Features().Disable("Copy") // macOS cloning is too fast for this test!
+		if r.Fremote.Features().IsLocal {
+			r.Fremote.Features().Disable("Copy") // macOS cloning is too fast for this test!
+		}
+	}
 	accounting.GlobalStats().ResetCounters()
 	startTime := time.Now()
 	err := Sync(ctx, r.Fremote, r.Flocal, false)
@@ -2022,6 +2028,14 @@ func TestMaxTransfer(t *testing.T) {
 		file3 := r.WriteFile("file3", string(make([]byte, 3*1024)), t1)
 		r.CheckLocalItems(t, file1, file2, file3)
 		r.CheckRemoteItems(t)
+
+		if runtime.GOOS == "darwin" {
+			// disable server-side copies as they don't count towards transfer size stats
+			r.Flocal.Features().Disable("Copy")
+			if r.Fremote.Features().IsLocal {
+				r.Fremote.Features().Disable("Copy")
+			}
+		}
 
 		accounting.GlobalStats().ResetCounters()
 
