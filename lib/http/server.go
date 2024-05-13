c0338bad3cbf44f12dde67097edc1b70f57e2add
Florian Klink <flokli@flokli.de>
2024-05-13 21:13:10 +0200

    lib/sd-activation: wrap coreos/go-systemd
    It fails to build on plan9, which is part of the rclone CI matrix, and
    the PR fixing it upstream doesn't seem to be getting traction.
    
    Stub it on our side, we can still remove this once it gets merged.

--- lib/http/server.go
+++ lib/http/server.go
@@ -18,11 +18,11 @@ import (
 	"sync"
 	"time"
 
-	sdActivation "github.com/coreos/go-systemd/v22/activation"
 	"github.com/go-chi/chi/v5"
 	"github.com/rclone/rclone/fs"
 	"github.com/rclone/rclone/fs/config/flags"
 	"github.com/rclone/rclone/lib/atexit"
+	sdActivation "github.com/rclone/rclone/lib/sdactivation"
 	"github.com/spf13/pflag"
 )
 
