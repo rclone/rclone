13ea77dd71bb7c8ff727f1f5375ab7a32b354a32
Nick Craig-Wood <nick@craig-wood.com>
2024-06-24 13:08:33 +0100

    nfsmount: allow tests to run on any unix where sudo mount/umount works

--- cmd/nfsmount/nfsmount_test.go
+++ cmd/nfsmount/nfsmount_test.go
@@ -1,14 +1,32 @@
-//go:build darwin && !cmount
+//go:build unix
 
 package nfsmount
 
 import (
+	"os/exec"
+	"runtime"
 	"testing"
 
 	"github.com/rclone/rclone/vfs/vfscommon"
 	"github.com/rclone/rclone/vfs/vfstest"
 )
 
+// Return true if the command ran without error
+func commandOK(name string, arg ...string) bool {
+	cmd := exec.Command(name, arg...)
+	_, err := cmd.CombinedOutput()
+	return err == nil
+}
+
 func TestMount(t *testing.T) {
+	if runtime.GOOS != "darwin" {
+		if !commandOK("sudo", "-n", "mount", "--help") {
+			t.Skip("Can't run sudo mount without a password")
+		}
+		if !commandOK("sudo", "-n", "umount", "--help") {
+			t.Skip("Can't run sudo umount without a password")
+		}
+		sudo = true
+	}
 	vfstest.RunTests(t, false, vfscommon.CacheModeMinimal, false, mount)
 }
