8d1d096c112ff26967496fd166697515fdf4b7b9
Nick Craig-Wood <nick@craig-wood.com>
2024-08-12 20:23:19 +0100

    drive: fix copying Google Docs to a backend which only supports SHA1
    When copying Google Docs to Backblaze B2 errors like this would happen
    
        ERROR : test.docx: Failed to calculate src hash: hash type not supported
        ERROR : test.docx: corrupted on transfer: sha1 hashes differ src
    
    This was due to an oversight in
    
    8fd66daab671bb98 drive: add support of SHA-1 and SHA-256 checksum
    
    Which omitted to change the base object (which includes Google Docs) so
    that it supported SHA-1 and SHA-256.

--- backend/drive/drive.go
+++ backend/drive/drive.go
@@ -3965,7 +3965,7 @@ func (o *Object) Hash(ctx context.Context, t hash.Type) (string, error) {
 	return "", hash.ErrUnsupported
 }
 func (o *baseObject) Hash(ctx context.Context, t hash.Type) (string, error) {
-	if t != hash.MD5 {
+	if t != hash.MD5 && t != hash.SHA1 && t != hash.SHA256 {
 		return "", hash.ErrUnsupported
 	}
 	return "", nil
