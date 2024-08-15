afd199d75614a4b049bb84459c3d352402df80aa
Florian Klink <flokli@flokli.de>
2024-08-15 23:31:21 +0300

    dlna: document external subtitle feature

--- cmd/serve/dlna/dlna.go
+++ cmd/serve/dlna/dlna.go
@@ -49,6 +49,10 @@ based on media formats or file extensions. Additionally, there is no
 media transcoding support. This means that some players might show
 files that they are not able to play back correctly.
 
+Rclone will add external subtitle files (.srt) to videos if they have the same
+filename as the video file itself (except the extension), either in the same
+directory as the video, or in a "Subs" subdirectory.
+
 ` + dlnaflags.Help + vfs.Help(),
 	Annotations: map[string]string{
 		"versionIntroduced": "v1.46",
