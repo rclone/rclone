943ea0acae5829bcdfe7e9cd22b991cd787a3f49
Nick Craig-Wood <nick@craig-wood.com>
2024-12-03 16:25:31 +0000

    chunker: mark ListP as not supported yet

--- backend/chunker/chunker.go
+++ backend/chunker/chunker.go
@@ -356,7 +356,8 @@ func NewFs(ctx context.Context, name, rpath string, m configmap.Mapper) (fs.Fs, 
 		DirModTimeUpdatesOnWrite: true,
 	}).Fill(ctx, f).Mask(ctx, baseFs).WrapsFs(f, baseFs)
 
-	f.features.Disable("ListR") // Recursive listing may cause chunker skip files
+	f.features.ListR = nil // Recursive listing may cause chunker skip files
+	f.features.ListP = nil // ListP not supported yet
 
 	return f, err
 }
