fa407a7f5ea36e7916d82cac04cbf76a6b817158
Benji Silver <git@benjifox.gay>
2025-04-18 12:02:48 +0200

    fix fs root
    Co-Authored-By: arch <47732882+x1arch@users.noreply.github.com>

--- backend/terabox/terabox.go
+++ backend/terabox/terabox.go
@@ -297,7 +297,7 @@ func (f *Fs) About(ctx context.Context) (*fs.Usage, error) {
 func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err error) {
 	debug(f.opt, 1, "List %s;", dir)
 
-	if f.origRootItem == nil {
+	if f.root != "/" && f.origRootItem == nil {
 		return nil, fs.ErrorDirNotFound
 	}
 
