c54869f65d46db92499b5aa2f57bfeba929e1791
kingston125 <support@filelu.com>
2025-04-30 13:52:23 -0400

    Add files via upload

--- backend/filelu/api/types.go
+++ backend/filelu/api/types.go
@@ -1,35 +1,70 @@
 // Package api defines types for interacting with the FileLu API.
 package api
 
-// FolderListResponse represents the response from the folder/list API.
-type FolderListResponse struct {
-	Status int    `json:"status"` // HTTP status code of the response.
-	Msg    string `json:"msg"`    // Message describing the response.
+import "encoding/json"
+
+// CreateFolderResponse represents the response for creating a folder.
+type CreateFolderResponse struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
 	Result struct {
-		Files   []FolderListFile   `json:"files"`   // List of files in the folder.
-		Folders []FolderListFolder `json:"folders"` // List of folders in the folder.
-	} `json:"result"` // Nested result structure containing files and folders.
+		FldID interface{} `json:"fld_id"`
+	} `json:"result"`
 }
 
-// FolderListFile represents a file in the FolderListResponse.
-type FolderListFile struct {
-	Name      string `json:"name"`      // File name.
-	Size      int64  `json:"size"`      // File size in bytes.
-	Uploaded  string `json:"uploaded"`  // Upload date as a string.
-	Thumbnail string `json:"thumbnail"` // URL to the file's thumbnail.
-	Link      string `json:"link"`      // URL to access the file.
-	FldID     string `json:"fld_id"`    // Folder ID containing the file.
-	FileCode  string `json:"file_code"` // Unique code for the file.
-	Hash      string `json:"hash"`      // Hash of the file for verification.
+// DeleteFolderResponse represents the response for deleting a folder.
+type DeleteFolderResponse struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
 }
 
-// FolderListFolder represents a folder in the FolderListResponse.
-type FolderListFolder struct {
-	Name      string `json:"name"`       // Folder name.
-	Code      string `json:"code"`       // Unique code for the folder.
-	FldID     string `json:"fld_id"`     // Folder ID.
-	FldPublic int    `json:"fld_public"` // Indicates if the folder is public.
-	Filedrop  int    `json:"filedrop"`   // Indicates if the folder supports file drop.
+// FolderListResponse represents the response for listing folders.
+type FolderListResponse struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
+	Result struct {
+		Files []struct {
+			Name     string      `json:"name"`
+			FldID    json.Number `json:"fld_id"`
+			Path     string      `json:"path"`
+			FileCode string      `json:"file_code"`
+			Size     int64       `json:"size"`
+		} `json:"files"`
+		Folders []struct {
+			Name  string      `json:"name"`
+			FldID json.Number `json:"fld_id"`
+			Path  string      `json:"path"`
+		} `json:"folders"`
+	} `json:"result"`
+}
+
+// FileDirectLinkResponse represents the response for a direct link to a file.
+type FileDirectLinkResponse struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
+	Result struct {
+		URL  string `json:"url"`
+		Size int64  `json:"size"`
+	} `json:"result"`
+}
+
+// FileInfoResponse represents the response for file information.
+type FileInfoResponse struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
+	Result []struct {
+		Size     string `json:"size"`
+		Name     string `json:"name"`
+		FileCode string `json:"filecode"`
+		Hash     string `json:"hash"`
+		Status   int    `json:"status"`
+	} `json:"result"`
+}
+
+// DeleteFileResponse represents the response for deleting a file.
+type DeleteFileResponse struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
 }
 
 // AccountInfoResponse represents the response for account information.
@@ -43,18 +78,4 @@ type AccountInfoResponse struct {
 		Storage       string `json:"storage"`        // Total storage available to the user.
 		StorageUsed   string `json:"storage_used"`   // Amount of storage used.
 	} `json:"result"` // Nested result structure containing account details.
-}
-
-// FolderDeleteResponse represents the response for deleting a folder.
-type FolderDeleteResponse struct {
-	Status     int    `json:"status"`      // HTTP status code of the response.
-	Msg        string `json:"msg"`         // Message describing the response.
-	Result     string `json:"result"`      // Result of the deletion operation.
-	ServerTime string `json:"server_time"` // Server timestamp of the operation.
-}
-
-// DeleteResponse represents the response for deleting a file or folder.
-type DeleteResponse struct {
-	Status int    `json:"status"` // HTTP status code of the response.
-	Msg    string `json:"msg"`    // Message describing the response.
 }
