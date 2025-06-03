f96e3704fb04f21c3f8c6dc5d9a9e0d9de4173db
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2025-06-03 10:46:20 -0600

    cmount: use constant to detect whether using fuse3 or not

--- cmd/cmount/mount_fuse3.go
+++ cmd/cmount/mount_fuse3.go
@@ -1,49 +1,6 @@
 //go:build fuse3 && (linux || freebsd)
 
 // Package cmount implements a FUSE mounting system for rclone remotes.
-//
-// FUSE3 mount options.
 package cmount
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/rclone/rclone/cmd/mountlib"
-	"github.com/rclone/rclone/vfs"
-)
-
-// mountOptions configures the options from the command line flags
-func mountOptions(VFS *vfs.VFS, device string, mountpoint string, opt *mountlib.Options) (options []string) {
-	// Options
-	options = []string{
-		"-o", fmt.Sprintf("attr_timeout=%g", time.Duration(opt.AttrTimeout).Seconds()),
-	}
-	if opt.DebugFUSE {
-		options = append(options, "-o", "debug")
-	}
-
-	options = append(options, "-o", "fsname="+device)
-	options = append(options, "-o", "subtype=rclone")
-	if opt.AllowOther {
-		options = append(options, "-o", "allow_other")
-	}
-	if opt.AllowRoot {
-		options = append(options, "-o", "allow_root")
-	}
-	if opt.DefaultPermissions {
-		options = append(options, "-o", "default_permissions")
-	}
-	if VFS.Opt.ReadOnly {
-		options = append(options, "-o", "ro")
-	}
-	//if opt.WritebackCache {
-	// FIXME? options = append(options, "-o", WritebackCache())
-	//}
-
-	for _, option := range opt.ExtraOptions {
-		options = append(options, "-o", option)
-	}
-	options = append(options, opt.ExtraFlags...)
-	return options
-}
+const isFuse3 = true
