3b3096c9404ac3aeba922e7012a33eca2e994b7d
Flora Thiebaut <johann.thiebaut@gmail.com>
2025-06-05 17:40:54 +0200

    doi: add new doi backend
    Add a new backend to support mounting datasets published with a digital
    object identifier (DOI).

---
+++ backend/doi/doi_test.go
@@ -0,0 +1,16 @@
+// Test DOI filesystem interface
+package doi
+
+import (
+	"testing"
+
+	"github.com/rclone/rclone/fstest/fstests"
+)
+
+// TestIntegration runs integration tests against the remote
+func TestIntegration(t *testing.T) {
+	fstests.Run(t, &fstests.Opt{
+		RemoteName: "TestDoi:",
+		NilObject:  (*Object)(nil),
+	})
+}
