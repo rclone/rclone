ffe93205e83efb5826e2c3efeb948728af2ef7bc
StarHack <danielschmidth@googlemail.com>
2025-06-05 19:17:57 +0200

    internxt: add integration test

--- backend/internxt/internxt_test.go
+++ backend/internxt/internxt_test.go
@@ -1,15 +1,47 @@
-package internxt_test
+package internxt
 
 import (
+	"context"
+	"errors"
 	"testing"
 
-	"github.com/rclone/rclone/backend/internxt"
+	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/fs/operations"
+	"github.com/rclone/rclone/fstest"
 	"github.com/rclone/rclone/fstest/fstests"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIntegration(t *testing.T) {
 	fstests.Run(t, &fstests.Opt{
 		RemoteName: "TestInternxt:",
-		NilObject:  (*internxt.Object)(nil),
+		NilObject:  (*Object)(nil),
 	})
+}
+
+// TestMakeDir verifies that basic operations (such as mkdir) can be performed
+func TestMakeDir(t *testing.T) {
+	const (
+		remoteName = "TestInternxt:"
+	)
+	ctx := context.Background()
+	fstest.Initialise()
+	subRemoteName, _, err := fstest.RandomRemoteName(remoteName)
+	require.NoError(t, err)
+	f, err := fs.NewFs(ctx, subRemoteName)
+	if errors.Is(err, fs.ErrorNotFoundInConfigFile) {
+		t.Logf("Didn't find %q in config file - skipping tests", remoteName)
+		return
+	}
+	require.NoError(t, err)
+
+	entr, err := f.List(ctx, "")
+	t.Log(entr)
+	require.NoError(t, err)
+
+	err = f.Mkdir(ctx, "hello-integration-test")
+	require.NoError(t, err)
+
+	// Tear down
+	require.NoError(t, operations.Purge(ctx, f, ""))
 }
