f7c533dc384db6f834ade71103aa7fecebdee197
StarHack <danielschmidth@googlemail.com>
2025-07-19 16:19:05 +0200

    internxt: remove second testcase for now

--- backend/internxt/internxt_test.go
+++ backend/internxt/internxt_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/rclone/rclone/backend/internxt"
-	"github.com/rclone/rclone/fstest"
 	"github.com/rclone/rclone/fstest/fstests"
 )
 
@@ -15,17 +14,5 @@ func TestIntegration(t *testing.T) {
 	fstests.Run(t, &fstests.Opt{
 		RemoteName: "TestInternxt:",
 		NilObject:  (*internxt.Object)(nil),
-	})
-}
-
-func TestIntegrationWithConfig(t *testing.T) {
-	if *fstest.RemoteName != "" {
-		t.Skip("Skipping as -remote set")
-	}
-	name := "TestInternxt"
-	fstests.Run(t, &fstests.Opt{
-		RemoteName:  name + ":",
-		NilObject:   (*internxt.Object)(nil),
-		ExtraConfig: []fstests.ExtraConfigItem{},
 	})
 }
