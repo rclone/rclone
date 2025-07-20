4b6dae23ef52420779d3c306063deed92fd9fe7b
StarHack <danielschmidth@googlemail.com>
2025-07-20 23:15:51 +0200

    internxt: add SkipTrailingDot flag since internxt doesnt support trailing dots

--- backend/internxt/internxt_test.go
+++ backend/internxt/internxt_test.go
@@ -9,7 +9,8 @@ import (
 // TestIntegration runs integration tests against the remote
 func TestIntegration(t *testing.T) {
 	fstests.Run(t, &fstests.Opt{
-		RemoteName:     "TestInternxt:",
-		SkipLeadingDot: true,
+		RemoteName:      "TestInternxt:",
+		SkipLeadingDot:  true,
+		SkipTrailingDot: true,
 	})
 }
