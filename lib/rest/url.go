1d91618d9ef16496d98f11a7d89b7cd59ef61e5d
Nick Craig-Wood <nick@craig-wood.com>
2025-09-02 16:13:28 +0100

    lib/rest: add URLPathEscapeAll to URL escape as many chars as possible

--- lib/rest/url.go
+++ lib/rest/url.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 // URLJoin joins a URL and a path returning a new URL
@@ -23,4 +24,25 @@ func URLPathEscape(in string) string {
 	var u url.URL
 	u.Path = in
 	return u.String()
+}
+
+// URLPathEscapeAll escapes URL path the in string using URL escaping rules
+//
+// It escapes every character except [A-Za-z0-9] and /
+func URLPathEscapeAll(in string) string {
+	var b strings.Builder
+	b.Grow(len(in) * 3) // worst case: every byte escaped
+	const hex = "0123456789ABCDEF"
+	for i := range len(in) {
+		c := in[i]
+		if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
+			(c >= '0' && c <= '9') || c == '/' {
+			b.WriteByte(c)
+		} else {
+			b.WriteByte('%')
+			b.WriteByte(hex[c>>4])
+			b.WriteByte(hex[c&0x0F])
+		}
+	}
+	return b.String()
 }
