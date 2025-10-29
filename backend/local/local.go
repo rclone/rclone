77553b8dd5d64411b6641a5ecc0b95be47e022a7
Adam Dinwoodie <me-and@users.noreply.github.com>
2025-10-29 17:00:25 +0000

    local: add --skip-specials to ignore special files
    Give users a way to explicitly acknowledge that pipes, sockets and block
    devices are to be ignored without warnings.
    
    This follows the precedent set in commit 6152bab28 (local: add
    --skip-links to suppress symlink warnings, 2017-07-21) for ignoring
    warnings about symlinks.

--- backend/local/local.go
+++ backend/local/local.go
@@ -116,6 +116,17 @@ points, as you explicitly acknowledge that they should be skipped.`,
 				Advanced: true,
 			},
 			{
+				Name: "skip_specials",
+				Help: `Don't warn about skipped pipes, sockets and device objects.
+
+This flag disables warning messages on skipped pipes, sockets and
+device objects, as you explicitly acknowledge that they should be
+skipped.`,
+				Default:  false,
+				NoPrefix: true,
+				Advanced: true,
+			},
+			{
 				Name: "zero_size_links",
 				Help: `Assume the Stat size of links is zero (and read them instead) (deprecated).
 
@@ -328,6 +339,7 @@ type Options struct {
 	FollowSymlinks    bool                 `config:"copy_links"`
 	TranslateSymlinks bool                 `config:"links"`
 	SkipSymlinks      bool                 `config:"skip_links"`
+	SkipSpecials      bool                 `config:"skip_specials"`
 	UTFNorm           bool                 `config:"unicode_normalization"`
 	NoCheckUpdated    bool                 `config:"no_check_updated"`
 	NoUNC             bool                 `config:"nounc"`
@@ -1246,7 +1258,9 @@ func (o *Object) Storable() bool {
 		}
 		return false
 	} else if mode&(os.ModeNamedPipe|os.ModeSocket|os.ModeDevice) != 0 {
-		fs.Logf(o, "Can't transfer non file/directory")
+		if !o.fs.opt.SkipSpecials {
+			fs.Logf(o, "Can't transfer non file/directory")
+		}
 		return false
 	} else if mode&os.ModeDir != 0 {
 		// fs.Debugf(o, "Skipping directory")
