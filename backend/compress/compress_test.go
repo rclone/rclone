8f74e7d3317a6d87876daa7b8757641992749c7d
Alex <64072843+A1ex3@users.noreply.github.com>
2025-11-04 21:50:56 +0700

    backend/compress: add zstd compression
    Added support for reading and writing zstd-compressed archives in seekable format
    using "github.com/klauspost/compress/zstd" and
    "github.com/SaveTheRbtz/zstd-seekable-format-go/pkg".
    
    Bumped Go version from 1.24.0 to 1.24.4 due to requirements of
    "github.com/SaveTheRbtz/zstd-seekable-format-go/pkg".

--- backend/compress/compress_test.go
+++ backend/compress/compress_test.go
@@ -48,7 +48,27 @@ func TestRemoteGzip(t *testing.T) {
 	opt.ExtraConfig = []fstests.ExtraConfigItem{
 		{Name: name, Key: "type", Value: "compress"},
 		{Name: name, Key: "remote", Value: tempdir},
-		{Name: name, Key: "compression_mode", Value: "gzip"},
+		{Name: name, Key: "mode", Value: "gzip"},
+		{Name: name, Key: "level", Value: "-1"},
+	}
+	opt.QuickTestOK = true
+	fstests.Run(t, &opt)
+}
+
+// TestRemoteZstd tests ZSTD compression
+func TestRemoteZstd(t *testing.T) {
+	if *fstest.RemoteName != "" {
+		t.Skip("Skipping as -remote set")
+	}
+	tempdir := filepath.Join(os.TempDir(), "rclone-compress-test-zstd")
+	name := "TestCompressZstd"
+	opt := defaultOpt
+	opt.RemoteName = name + ":"
+	opt.ExtraConfig = []fstests.ExtraConfigItem{
+		{Name: name, Key: "type", Value: "compress"},
+		{Name: name, Key: "remote", Value: tempdir},
+		{Name: name, Key: "mode", Value: "zstd"},
+		{Name: name, Key: "level", Value: "2"},
 	}
 	opt.QuickTestOK = true
 	fstests.Run(t, &opt)
