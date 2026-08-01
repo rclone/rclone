834d685bd3ff2c3e9cbb92c9956ecdbc1f9b7940
smailzhu <9101328+smailzhu@users.noreply.github.com>
2026-08-01 15:53:16 +0000

    smugmug: add integration test harness

---
+++ backend/smugmug/smugmug_test.go
@@ -0,0 +1,17 @@
+// Test SmugMug filesystem interface
+package smugmug_test
+
+import (
+	"testing"
+
+	"github.com/rclone/rclone/backend/smugmug"
+	"github.com/rclone/rclone/fstest/fstests"
+)
+
+// TestIntegration runs integration tests against the remote.
+func TestIntegration(t *testing.T) {
+	fstests.Run(t, &fstests.Opt{
+		RemoteName: "TestSmugMug:",
+		NilObject:  (*smugmug.Object)(nil),
+	})
+}
